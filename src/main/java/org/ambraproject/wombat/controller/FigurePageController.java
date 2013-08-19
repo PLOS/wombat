1c98edb293d11a8f9d34b55b93124d59ae4b8dac
Ryan Skonnord <rskonnord@plos.org>
2013-08-19 16:29:15 -0700

    Basic use of Rhino's "figures" attribute in Figures page

--- src/main/java/org/ambraproject/wombat/controller/FigurePageController.java
+++ src/main/java/org/ambraproject/wombat/controller/FigurePageController.java
@@ -1,5 +1,7 @@
 package org.ambraproject.wombat.controller;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
 import com.google.common.io.Closer;
 import org.ambraproject.wombat.service.ArticleNotFoundException;
 import org.ambraproject.wombat.service.EntityNotFoundException;
@@ -16,6 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -23,6 +26,35 @@ public class FigurePageController {
 
   @Autowired
   private SoaService soaService;
+
+  /*
+   * Dereference keys given in "article.figures" to find assets from "article.assets".
+   * Set them up in a new object that can be accessed more easily from FreeMarker.
+   *
+   * TODO: Do this entirely in FreeMarker?
+   */
+  private static List<?> buildFigureList(Map<?, ?> articleMetadata) {
+    Map<?, ?> assets = (Map<?, ?>) articleMetadata.get("assets");
+    List<Map<?, ?>> figureMetadataList = (List<Map<?, ?>>) articleMetadata.get("figures");
+
+    List<Object> figures = Lists.newArrayListWithCapacity(figureMetadataList.size());
+    for (Map<?, ?> figureMetadata : figureMetadataList) {
+      Map<?, ?> asset = (Map<?, ?>) assets.get(figureMetadata.get("id"));
+      Object originalAsset = asset.get(figureMetadata.get("original"));
+
+      List<String> thumbnailAssetIds = (List<String>) figureMetadata.get("thumbnails");
+      List<Object> thumbnailAssets = Lists.newArrayListWithCapacity(thumbnailAssetIds.size());
+      for (String thumbnailAssetId : thumbnailAssetIds) {
+        Object thumbnailAsset = asset.get(thumbnailAssetId);
+        thumbnailAssets.add(thumbnailAsset);
+      }
+
+      Object figure = ImmutableMap.of("original", originalAsset, "thumbnails", thumbnailAssets);
+      figures.add(figure);
+    }
+
+    return figures;
+  }
 
   @RequestMapping("/{journal}/article/figures")
   public String renderFigurePage(Model model,
@@ -36,6 +68,7 @@ public class FigurePageController {
       throw new ArticleNotFoundException(articleId);
     }
     model.addAttribute("article", articleMetadata);
+    model.addAttribute("figures", buildFigureList(articleMetadata));
 
     return journal + "/ftl/article/figures";
   }
--- src/main/webapp/WEB-INF/views/ftl/article/figures.ftl
+++ src/main/webapp/WEB-INF/views/ftl/article/figures.ftl
@@ -8,7 +8,24 @@
 <div id="container-main">
 <#include "backToArticle.ftl" />
 
-<#-- TODO: Import and wire content -->
+  <section id="figures-content" class="content">
+  <#list figures as figure>
+    <figure class="figure-small">
+
+      <figcaption>
+      ${figure.original.title} <a>More »</a>
+      </figcaption>
+
+      <a class="figure-link">
+        <img class="figure-image" src="asset?id=${figure.thumbnails[0].doi}.${figure.thumbnails[0].extension}"
+             alt="${figure.original.title}">
+        <span class="figure-expand">Expand</span>
+      </a>
+
+    </figure>
+  </#list>
+
+  </section><#--end content-->
 
 <#include "../common/bottomMenu/bottomMenu.ftl" />
 

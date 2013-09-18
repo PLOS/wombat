971fc1d7ee7827056fcc7a1021a801860975e0fe
John Callaway <jcallaway@plos.org>
2013-09-18 15:45:36 -0700

    Site-specific home pages.

--- src/main/java/org/ambraproject/wombat/controller/HomeController.java
+++ src/main/java/org/ambraproject/wombat/controller/HomeController.java
@@ -1,21 +1,20 @@
 package org.ambraproject.wombat.controller;
 
-import org.ambraproject.wombat.service.SoaService;
+import org.ambraproject.wombat.config.Site;
+import org.ambraproject.wombat.config.SiteSet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.Locale;
-import java.util.Map;
 
 /**
- * Handles requests for the application home page.
+ * Handles requests for a site home page.
  */
 @Controller
 public class HomeController {
@@ -23,25 +22,17 @@ public class HomeController {
   private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
   @Autowired
-  private SoaService soaService;
+  private SiteSet siteSet;
 
   /**
    * Simply selects the home view to render by returning its name.
    */
-  @RequestMapping(value = "/", method = RequestMethod.GET)
-  public String home(Locale locale, Model model) throws Exception {
+  @RequestMapping(value = "/{site}/", method = RequestMethod.GET)
+  public String home(Locale locale, Model model, @PathVariable("site") String siteParam) throws Exception {
     logger.info("Welcome home! The client locale is {}.", locale);
 
-    Date date = new Date();
-    DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-
-    String formattedDate = dateFormat.format(date);
-    model.addAttribute("serverTime", formattedDate);
-
-    Map<?, ?> serverConfig = soaService.requestObject("config", Map.class);
-    model.addAttribute("testObject", serverConfig);
-
-    return "PlosOne/home";
+    Site site = siteSet.getSite(siteParam);
+    return site.getKey() + "/ftl/home";
   }
 
 }

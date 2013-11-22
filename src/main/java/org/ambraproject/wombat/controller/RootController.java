e5140fd901813c23cd0bdffd804c0bd79722da5d
Ryan Skonnord <rskonnord@plos.org>
2013-11-21 17:24:39 -0800

    Very imporant, critical feature (of a wombat picture on the root page)

--- src/main/java/org/ambraproject/wombat/controller/RootController.java
+++ src/main/java/org/ambraproject/wombat/controller/RootController.java
@@ -1,11 +1,18 @@
 package org.ambraproject.wombat.controller;
 
+import com.google.common.io.Closer;
 import org.ambraproject.wombat.config.SiteSet;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.ServletContext;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Handles requests to the application root.
@@ -15,6 +22,23 @@ public class RootController {
 
   @Autowired
   private SiteSet siteSet;
+
+  @Autowired
+  private ServletContext servletContext;
+
+  private String getResourceAsBase64(String path) throws IOException {
+    Closer closer = Closer.create();
+    byte[] bytes;
+    try {
+      InputStream stream = closer.register(servletContext.getResourceAsStream(path));
+      bytes = IOUtils.toByteArray(stream);
+    } catch (Throwable t) {
+      throw closer.rethrow(t);
+    } finally {
+      closer.close();
+    }
+    return Base64.encodeBase64String(bytes);
+  }
 
   /**
    * Show a page in response to the application root.
@@ -26,6 +50,13 @@ public class RootController {
   @RequestMapping(value = "/", method = RequestMethod.GET)
   public String home(Model model) throws Exception {
     model.addAttribute("siteKeys", siteSet.getSiteKeys());
+
+    /*
+     * Must supply this image data embedded (if at all), because there's no URL at which we could put it
+     * without colliding with the site namespace.
+     */
+    model.addAttribute("imageCode", getResourceAsBase64("/WEB-INF/views/app/wombat.jpg"));
+
     return "//approot";
   }
 
--- src/main/webapp/WEB-INF/views/app/approot.ftl
+++ src/main/webapp/WEB-INF/views/app/approot.ftl
@@ -16,5 +16,23 @@
 </#list>
 </ul>
 
+<#if imageCode??>
+<hr/>
+<div>
+<#-- Must embed the image data this way instead of having it at an href.
+     There's no URL at which we could put the image without colliding with the site namespace.
+  -->
+  <img src="data:image/jpg;base64,${imageCode}"/>
+</div>
+<div style="font-size: small">
+  Image:
+  <a href="http://commons.wikimedia.org/wiki/File:Wombat_1.jpg">"Southern Hairy-nosed Wombat (<i>Lasiorhinus
+    latifrons</i>)"</a>
+  by <a href="http://www.flickr.com/photos/stygiangloom/220992362/">Stygiangloom</a>.
+  Available under the
+  <a href="http://creativecommons.org/licenses/by/2.0/deed.en">Creative Commons Attribution 2.0 Generic License.</a>
+</div>
+</#if>
+
 </body>
 </html>

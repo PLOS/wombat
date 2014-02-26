1c999a92c19907534821251fa49d5f560fe7cb48
Ryan Skonnord <rskonnord@plos.org>
2014-02-26 13:33:51 -0800

    Add comments to RandomIntegerDirective
    Also tweaked some primitive wrappers.

--- src/main/java/org/ambraproject/wombat/freemarker/RandomIntegerDirective.java
+++ src/main/java/org/ambraproject/wombat/freemarker/RandomIntegerDirective.java
@@ -13,14 +13,26 @@ import java.io.IOException;
 import java.util.Map;
 import java.util.Random;
 
+/**
+ * Generates an insecure random integer, between the params "minValue" (inclusive) and "maxValue" (exclusive).
+ */
 public class RandomIntegerDirective implements TemplateDirectiveModel {
 
   private static final Integer DEFAULT_MIN = 0;
   private static final Integer DEFAULT_MAX = 10000;
 
-  private final Random random = new Random();
+  private final Random random = new Random(); // needs SecureRandom if these numbers are used cryptographically
 
-  private static Integer asInteger(Object number, Integer defaultValue) throws TemplateModelException {
+  /**
+   * Interpret a FreeMarker number as a primitive integer.
+   *
+   * @param number       a FreeMarker number value
+   * @param defaultValue the value to use if {@code number == null}
+   * @return the integer value
+   * @throws TemplateModelException
+   * @throws java.lang.ClassCastException if {@code number} is non-null and not a FreeMarker number
+   */
+  private static int asInteger(Object number, int defaultValue) throws TemplateModelException {
     if (number == null) return defaultValue;
     return ((TemplateNumberModel) number).getAsNumber().intValue();
   }
@@ -28,8 +40,8 @@ public class RandomIntegerDirective implements TemplateDirectiveModel {
   @Override
   public void execute(Environment env, Map params, TemplateModel[] loopVars, TemplateDirectiveBody body)
       throws TemplateException, IOException {
-    Integer minValue = asInteger(params.get("minValue"), DEFAULT_MIN);
-    Integer maxValue = asInteger(params.get("maxValue"), DEFAULT_MAX);
+    int minValue = asInteger(params.get("minValue"), DEFAULT_MIN);
+    int maxValue = asInteger(params.get("maxValue"), DEFAULT_MAX);
     Preconditions.checkArgument(minValue < maxValue);
 
     int randomValue = random.nextInt(maxValue - minValue) + minValue;

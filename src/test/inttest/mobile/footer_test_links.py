8396b843a01588fab7828918548fb8c1e2769efd
Jeffrey Gray PLOS <jgray@plos.org>
2014-03-18 17:11:28 -0700

    Updated POM file to reference the non-snapshot rhombat Updated the inttests to conform to standards a bit more

--- src/test/inttest/mobile/footer_test_links.py
+++ src/test/inttest/mobile/footer_test_links.py
@@ -4,62 +4,64 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 import unittest, time, re
+from resources import base_url
+from resources import journal_key
 
 class FooterTestLinks(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(30)
-        self.base_url = "http://one-fluffy.plosjournals.org/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
-    
-    def test_footer_test_links(self):
-        driver = self.driver
-        driver.get(self.base_url + "/wombat/PlosOne/")
-        try: self.assertEqual("Back to Top", driver.find_element_by_css_selector("span.btn-text").text)
-        except AssertionError as e: self.verificationErrors.append(str(e))
-        try: self.assertEqual("About Us", driver.find_element_by_link_text("About Us").text)
-        except AssertionError as e: self.verificationErrors.append(str(e))
-        try: self.assertEqual("Full Site", driver.find_element_by_link_text("Full Site").text)
-        except AssertionError as e: self.verificationErrors.append(str(e))
-        try: self.assertEqual("Feedback", driver.find_element_by_link_text("Feedback").text)
-        except AssertionError as e: self.verificationErrors.append(str(e))
-        try: self.assertEqual("Internet Systems Consortium.", driver.find_element_by_link_text("Internet Systems Consortium.").text)
-        except AssertionError as e: self.verificationErrors.append(str(e))
-        try: self.assertEqual("Privacy Policy", driver.find_element_by_link_text("Privacy Policy").text)
-        except AssertionError as e: self.verificationErrors.append(str(e))
-        try: self.assertEqual("Terms of Use", driver.find_element_by_link_text("Terms of Use").text)
-        except AssertionError as e: self.verificationErrors.append(str(e))
-        try: self.assertEqual("Media Inquiries", driver.find_element_by_link_text("Media Inquiries").text)
-        except AssertionError as e: self.verificationErrors.append(str(e))
-        driver.find_element_by_link_text("Full Site").click()
-        try: self.assertTrue(self.is_element_present(By.LINK_TEXT, "create account"))
-        except AssertionError as e: self.verificationErrors.append(str(e))
-    
-    def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
-        except NoSuchElementException, e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.driver.switch_to_alert()
-        except NoAlertPresentException, e: return False
-        return True
-    
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally: self.accept_next_alert = True
-    
-    def tearDown(self):
-        self.driver.quit()
-        self.assertEqual([], self.verificationErrors)
+  def setUp(self):
+    self.driver = webdriver.Firefox()
+    self.driver.implicitly_wait(30)
+    self.base_url = base_url
+    self.verificationErrors = []
+    self.accept_next_alert = True
+  
+  def test_footer_test_links(self):
+    driver = self.driver
+    driver.get(self.base_url + "/wombat/" + journal_key + "/")
+    try: self.assertEqual("Back to Top", driver.find_element_by_css_selector("span.btn-text").text)
+    except AssertionError as e: self.verificationErrors.append(str(e))
+    try: self.assertEqual("About Us", driver.find_element_by_link_text("About Us").text)
+    except AssertionError as e: self.verificationErrors.append(str(e))
+    try: self.assertEqual("Full Site", driver.find_element_by_link_text("Full Site").text)
+    except AssertionError as e: self.verificationErrors.append(str(e))
+    try: self.assertEqual("Feedback", driver.find_element_by_link_text("Feedback").text)
+    except AssertionError as e: self.verificationErrors.append(str(e))
+    try: self.assertEqual("Internet Systems Consortium.", driver.find_element_by_link_text("Internet Systems Consortium.").text)
+    except AssertionError as e: self.verificationErrors.append(str(e))
+    try: self.assertEqual("Privacy Policy", driver.find_element_by_link_text("Privacy Policy").text)
+    except AssertionError as e: self.verificationErrors.append(str(e))
+    try: self.assertEqual("Terms of Use", driver.find_element_by_link_text("Terms of Use").text)
+    except AssertionError as e: self.verificationErrors.append(str(e))
+    try: self.assertEqual("Media Inquiries", driver.find_element_by_link_text("Media Inquiries").text)
+    except AssertionError as e: self.verificationErrors.append(str(e))
+    driver.find_element_by_link_text("Full Site").click()
+    try: self.assertTrue(self.is_element_present(By.LINK_TEXT, "create account"))
+    except AssertionError as e: self.verificationErrors.append(str(e))
+  
+  def is_element_present(self, how, what):
+    try: self.driver.find_element(by=how, value=what)
+    except NoSuchElementException, e: return False
+    return True
+  
+  def is_alert_present(self):
+    try: self.driver.switch_to_alert()
+    except NoAlertPresentException, e: return False
+    return True
+  
+  def close_alert_and_get_its_text(self):
+    try:
+      alert = self.driver.switch_to_alert()
+      alert_text = alert.text
+      if self.accept_next_alert:
+        alert.accept()
+      else:
+        alert.dismiss()
+      return alert_text
+    finally: self.accept_next_alert = True
+  
+  def tearDown(self):
+    self.driver.quit()
+    self.assertEqual([], self.verificationErrors)
 
 if __name__ == "__main__":
-    unittest.main()
+  unittest.main()

89f7da641ef608cc6f18359aa0234cff2b80cd75
Ryan Skonnord <rskonnord@plos.org>
2014-04-22 14:30:59 -0700

    Improve default character encoding in ReaderService
    Previously, threw an NPE if the response had a Content-Type with no
    charset, and used Apache's default of ISO-8859-1 if there was no
    Content-Type at all. Instead, always default to UTF-8 and log a warning.

--- src/main/java/org/ambraproject/wombat/service/remote/ReaderService.java
+++ src/main/java/org/ambraproject/wombat/service/remote/ReaderService.java
@@ -1,8 +1,12 @@
 package org.ambraproject.wombat.service.remote;
 
+import com.google.common.base.Preconditions;
+import org.apache.commons.io.Charsets;
 import org.apache.http.HttpEntity;
 import org.apache.http.conn.HttpClientConnectionManager;
 import org.apache.http.entity.ContentType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -10,14 +14,34 @@ import java.io.Reader;
 import java.nio.charset.Charset;
 
 public class ReaderService extends AbstractRemoteService<Reader> {
+  private static final Logger log = LoggerFactory.getLogger(ReaderService.class);
 
   public ReaderService(HttpClientConnectionManager connectionManager) {
     super(connectionManager);
   }
 
+  /*
+   * Default for converting the response's byte stream to characters.
+   * Preferred over java.nio.charset.Charset.defaultCharset() to avoid an environmental dependency.
+   */
+  private static final Charset DEFAULT_CHARSET = Charsets.UTF_8;
+
+  private static Charset getCharsetOrDefault(HttpEntity entity) {
+    Charset charset = null;
+    ContentType contentType = ContentType.get(Preconditions.checkNotNull(entity));
+    if (contentType != null) {
+      charset = contentType.getCharset(); // may be null
+    }
+    if (charset == null) {
+      log.warn("Charset not specified in response header; defaulting to {}", DEFAULT_CHARSET.name());
+      charset = DEFAULT_CHARSET;
+    }
+    return charset;
+  }
+
   @Override
   public Reader open(HttpEntity entity) throws IOException {
-    Charset charset = ContentType.getOrDefault(entity).getCharset();
+    Charset charset = getCharsetOrDefault(entity);
     return new InputStreamReader(entity.getContent(), charset);
   }
 

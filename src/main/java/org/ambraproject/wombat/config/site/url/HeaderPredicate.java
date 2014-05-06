5cda40585ad586cf30b4c039f7826a6953919409
Ryan Skonnord <rskonnord@plos.org>
2014-05-06 11:11:12 -0700

    DPRO-21: Refactor SiteRequestScheme to pull in site-building logic
    The buildLink part of the former interface does not compose as cleanly as
    the isForSite method. We want to combine several interfaces that describe
    the site definition -- path token plus headers -- though only the former
    affects the way links are built.
    
    As of this commit, assume that the class is concerned only with building
    absolute links to pages within the same site. Therefore, only the "path
    token" part of definition needs special handling, which is done in the
    buildLink method of the SiteRequestScheme path definition. The "is for
    site" logic remains as an interface (SiteRequestPredicate) that can be
    composed with simple "and" logic.

---
+++ src/main/java/org/ambraproject/wombat/config/site/url/HeaderPredicate.java
@@ -0,0 +1,57 @@
+package org.ambraproject.wombat.config.site.url;
+
+import com.google.common.base.Preconditions;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+
+class HeaderPredicate implements SiteRequestPredicate {
+
+  private final String headerName;
+  private final String requiredValue;
+
+  HeaderPredicate(String headerName, String requiredValue) {
+    this.headerName = Preconditions.checkNotNull(headerName);
+    this.requiredValue = Preconditions.checkNotNull(requiredValue);
+  }
+
+  @Override
+  public boolean isForSite(HttpServletRequest request) {
+    Enumeration headers = request.getHeaders(headerName);
+    while (headers.hasMoreElements()) {
+      if (requiredValue.equalsIgnoreCase((String) headers.nextElement())) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  @Override
+  public String toString() {
+    return "HeaderPredicate{" +
+        "headerName='" + headerName + '\'' +
+        ", requiredValue='" + requiredValue + '\'' +
+        '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    HeaderPredicate that = (HeaderPredicate) o;
+
+    if (!headerName.equals(that.headerName)) return false;
+    if (!requiredValue.equals(that.requiredValue)) return false;
+
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = headerName.hashCode();
+    result = 31 * result + requiredValue.hashCode();
+    return result;
+  }
+
+}

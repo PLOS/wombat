66ab368a395b28a8a34f97c8a0ad7e2bc60f109a
Ryan Skonnord <rskonnord@plos.org>
2014-05-19 15:36:09 -0700

    Remove old Subversion wildcards

--- src/main/java/org/ambraproject/wombat/service/remote/JsonService.java
+++ src/main/java/org/ambraproject/wombat/service/remote/JsonService.java
@@ -1,10 +1,15 @@
 /*
- * $HeadURL$
- * $Id$
- * Copyright (c) 2006-2013 by Public Library of Science http://plos.org http://ambraproject.org
+ * Copyright (c) 2006-2014 by Public Library of Science
+ * http://plos.org
+ * http://ambraproject.org
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0Unless required by applicable law or agreed to in writing, software
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and

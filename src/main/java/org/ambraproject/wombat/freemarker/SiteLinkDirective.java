8b80e37984dcb75ad5a744e346f26466bc36c0d0
Ryan Skonnord <rskonnord@plos.org>
2014-05-28 12:10:29 -0700

    Add mode to SiteLinkDirective for assigning link to loopvar

--- src/main/java/org/ambraproject/wombat/freemarker/SiteLinkDirective.java
+++ src/main/java/org/ambraproject/wombat/freemarker/SiteLinkDirective.java
@@ -5,6 +5,7 @@ import freemarker.template.TemplateDirectiveBody;
 import freemarker.template.TemplateDirectiveModel;
 import freemarker.template.TemplateException;
 import freemarker.template.TemplateModel;
+import freemarker.template.TemplateModelException;
 import freemarker.template.TemplateScalarModel;
 import org.ambraproject.wombat.controller.SiteResolver;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,18 @@ import java.util.Map;
 
 /**
  * Formats a path, relative to the root of a site, into an absolute link.
+ * <p/>
+ * If there is no loop var, render the link as the output of the macro. For example:
+ * <pre>
+ *   <link rel="stylesheet" type="text/css" href="<@siteLink path="resource/css/screen.css" />"/>
+ * </pre>
+ * The directive may be invoked with one loop var, in which case the directive will assign the link to that variable and
+ * render the body with it. For example:
+ * <pre>
+ *   <@siteLink path="resource/css/screen.css" ; link>
+ *     <link rel="stylesheet" type="text/css" href="${link}"/>
+ *   </@siteLink>
+ * </pre>
  */
 public class SiteLinkDirective implements TemplateDirectiveModel {
 
@@ -29,7 +42,15 @@ public class SiteLinkDirective implements TemplateDirectiveModel {
     }
     String path = ((TemplateScalarModel) pathObj).getAsString();
     String link = new SitePageContext(siteResolver, env).buildLink(path);
-    env.getOut().write(link);
+
+    if (loopVars.length == 0) {
+      env.getOut().write(link);
+    } else if (loopVars.length == 1) {
+      loopVars[0] = env.getObjectWrapper().wrap(link);
+      body.render(env.getOut());
+    } else {
+      throw new TemplateModelException("SiteLinkDirective does not take more than 1 loopVar");
+    }
   }
 
 }

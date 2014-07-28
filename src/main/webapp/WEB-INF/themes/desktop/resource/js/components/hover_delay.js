6365aa8df1109701e9a2168477cab1072d6e5a71
Polina Grinbaum <pgrinbaum@plos.org>
2014-07-28 12:11:10 -0700

    adding touch specific behavior to hover_dealy
    This effects the pull down menu

--- src/main/webapp/WEB-INF/themes/desktop/resource/js/components/hover_delay.js
+++ src/main/webapp/WEB-INF/themes/desktop/resource/js/components/hover_delay.js
@@ -5,20 +5,29 @@
  */
 
 
-  hover_delay = {
-    init: function () {
-      // kick things off
-      //HoverIntent.js is used for the main navigation delay on hover
+hover_delay = {
 
-      function showIt() {
-        $(this).addClass("hover");
-        $('.dropdown', this).css('visibility', 'visible');
-      }
-      function hideIt() {
-        $(this).removeClass("hover");
-        $('.dropdown', this).css('visibility', 'hidden');
-      }
+  init: function () {
+    // kick things off
 
+    //HoverIntent.js is used for the main navigation delay on hover
+    function showIt() {
+      $(this).addClass("hover");
+      $('.dropdown', this).css('visibility', 'visible');
+    }
+
+    function hideIt() {
+      $(this).removeClass("hover");
+      $('.dropdown', this).css('visibility', 'hidden');
+    }
+
+    // if mobile, use modernizer to check for touch events. If so then:
+//      1. add needsclick class so fastclick won't do it's magic
+//      2. use hover instead of hoverIntent - hoverIntent adds timign to the hover we dont' want for touch devices. '
+    if ($('html.touch').length) {
+      $('li.has-dropdown').addClass('needsclick').hover(showIt, hideIt);
+    } else {
       $('li.has-dropdown').hoverIntent(showIt, hideIt);
     }
-  };
+  }
+}

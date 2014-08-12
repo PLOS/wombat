f5f03e9e0947b4672c046f59e079e2e4518dea21
Polina Grinbaum <pgrinbaum@plos.org>
2014-08-12 15:56:31 -0700

    DPRO-297 - pulled menu code into it's own file - renamed variable name

--- src/main/webapp/WEB-INF/themes/desktop/resource/js/components/hover_delay.js
+++ src/main/webapp/WEB-INF/themes/desktop/resource/js/components/hover_delay.js
@@ -2,61 +2,37 @@
  * Created by pgrinbaum on 7/17/
 
  * requires:  hoverIntent
+ * requires: menu_drop
  */
 
-//  Menu Function
-(function ($) {
-
-  $.fn.menuDrop = function (action, options) {
-    options = $.extend({}, defaults, options);
-
-    var defaults = {
-      child: '.dropdown'
-    };
-
-    if (action === "show") {
-      this.addClass("hover");
-      $(options.child, this).css('visibility', 'visible');
-      return this;
-    }
-
-    if (action === "hide") {
-      this.removeClass("hover");
-      $(options.child, this).css('visibility', 'hidden');
-      return this;
-    }
-
-  };
-
-}(jQuery));
 
 hover_delay = {
 
   init: function () {
     // kick things off
-    var $dropdown = $('li.has-dropdown');
+    var $menu_drop_selector = $('li.has-dropdown');
     // if mobile, use modernizer to check for touch events. If so then:
 //      1. add needsclick class so fastclick won't do it's magic
 //      2. use hover instead of hoverIntent - hoverIntent adds timing to the hover we don't want for touch devices. '
     // 3. we need to invoke the hide action AFTER the click because safari on mobile will persist hover state when you go to another page.
 
     if ($('html.touch').length) {
-      $dropdown.
+      $menu_drop_selector.
           addClass('needsclick').
           hover(
-            function () {$(this).menuDrop("show");},
-            function () {$(this).menuDrop("hide");}
+            function () {$(this).menu_drop("show");},
+            function () {$(this).menu_drop("hide");}
           );
       // invoke the hide menu
       $('.dropdown a').on({ 'click':
           function () {
-            $.Deferred().done($dropdown.menuDrop("hide"));}
+            $.Deferred().done($menu_drop_selector.menu_drop("hide"));}
       });
     } else {
       //HoverIntent.js is used for the main navigation delay on hover
-      $dropdown.hoverIntent(
-          function () {$(this).menuDrop("show");},
-          function () {$(this).menuDrop("hide");}
+      $menu_drop_selector.hoverIntent(
+          function () {$(this).menu_drop("show");},
+          function () {$(this).menu_drop("hide");}
       );
     }
   }

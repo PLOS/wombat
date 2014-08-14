7545c3bdd1610f9230f9340abf05822e516ff1ef
DanaDowell <ddowell@plos.org>
2014-08-14 15:19:15 -0700

    DPRO-320: show published date

--- src/main/webapp/WEB-INF/themes/desktop/resource/js/pages/article.js
+++ src/main/webapp/WEB-INF/themes/desktop/resource/js/pages/article.js
@@ -1,6 +1,14 @@
 /**
  * Created by ddowell on 8/14/14.
  */
-//* requires dateparse.js
 
-var articlePubDate = dateParse(entry.publishedDate);
+(function ($) {
+
+  var rawDate = document.getElementById("rawPubDate").value;
+  var articlePubDate = dateParse(rawDate, true);
+
+  $( document ).ready(function() {
+    $("#publishedDate").append(articlePubDate);
+  });
+
+}(jQuery));

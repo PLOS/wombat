38512650577f46dcdfc71a76e306848cf373f064
Polina Grinbaum <pgrinbaum@plos.org>
2014-09-03 10:05:51 -0700

    checking in a comment

--- src/main/webapp/WEB-INF/themes/desktop/resource/js/components/toggle.js
+++ src/main/webapp/WEB-INF/themes/desktop/resource/js/components/toggle.js
@@ -20,7 +20,7 @@ toggle = {
     var s=this.settings;
     $(s.toggle_hidden).hide();
     $(s.toggle_trigger).on('click', function(){
-      $(this).siblings(s.toggle_trigger).andSelf().toggle(s.speed);
+      $(this).siblings(s.toggle_trigger).andSelf().toggle(s.speed); //TODO: don't repeat myself so much here.
       $(this).siblings(s.toggle_target).toggle(s.speed);
       $(this).siblings(s.toggle_add).toggle(s.speed);
     })

e6be9d3995df501f96c5b8d3191a514f520e59c9
danadowell <ddowell@plos.org>
2014-09-12 09:22:10 -0700

    DPRO-371: do the author list truncation with javascript

--- src/main/webapp/WEB-INF/themes/desktop/resource/js/pages/article.js
+++ src/main/webapp/WEB-INF/themes/desktop/resource/js/pages/article.js
@@ -1,47 +1,74 @@
-/**
- * Created by ddowell on 8/14/14.
- */
 
 (function ($) {
 
-  var rawDate = document.getElementById("rawPubDate").value,
-  articlePubDate = dateParse(rawDate, true);
+  var floatHeader, floater,
+  //parse xml date into legible date
+    rawDate = document.getElementById("rawPubDate").value,
+    articlePubDate = dateParse(rawDate, true);
 
   $( document ).ready(function() {
-   //cache the floating title div
-    var floater = $("#floatTitleTop");
 
-    $(window).on('scroll', function (){
-      //if floater has been removed, no need to run function
-      if (floater) {
-          onscroll();
+    $("#artPubDate").append(articlePubDate);
+
+    floater = $("#floatTitleTop");
+
+    showFloater = function() {
+      var top = $(window).scrollTop(),
+        topLimit = 420;
+
+      if ( top > topLimit ) {
+        $(floater).addClass('topVisible');
+        floatHeader();
+
+      } else if ( top < topLimit ) {
+        $(floater).removeClass('topVisible');
       }
+
+    };
+    floatHeader = function(){
+      var floaterList, floaterListEl, floaterListHeight, toTruncate, listEnding;
+
+      floaterList = $("#floatAuthorList");
+      //if the floating header has been deleted, don't run overflown()
+      if (floaterList.length > 0) {
+        toTruncate = $(floaterList).overflown();
+
+        if (toTruncate === true) {
+          floaterListEl = document.getElementById("floatAuthorList");
+          floaterListHeight = floaterListEl.offsetTop;
+          listEnding = "<li>&hellip;</li>";
+
+          $(floaterList).children().each(function () {
+            if (this.offsetTop > floaterListHeight) {
+              $(this).addClass("remove");
+            }
+          });
+          $(".remove").remove();
+          $(floaterList).append(listEnding);
+        }
+      }
+    };
+    if ( $(floater).length > 0 ) {
+      $(window).on('scroll', function () {
+        showFloater();
+      });
+    }
+
+    $(floater).find('.logo-close').on('click', function () {
+      $(floater).remove();
+      floater = null;
     });
 
-  $("#artPubDate").append(articlePubDate);
-
-//show title & authors on floating header if window scrolls to tabs area
-    onscroll = function() {
-      var top = $(window).scrollTop(),
-          topLimit = 420;
-
-        if ( top > topLimit ) {
-          $(floater).addClass('topVisible');
-
-          $(floater).find('.logo-close').on('click', function () {
-            $(floater).remove();
-          });
-
-        } else if ( top < topLimit ) {
-          $(floater).removeClass('topVisible');
-        }
-    };
     // initialize toggle for author list view more
     toggle.init();
     // initialize tooltip for author info
-    tooltip.init(); 
+    tooltip.init();
   });
 
+  $.fn.overflown = function(){
+    var e = this[0];
+    return e.scrollHeight > e.clientHeight;
+  }
 
 }(jQuery));
 

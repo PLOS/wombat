f15a1b1b47baf461555d6fd941758f245fe2689e
Polina Grinbaum <pgrinbaum@plos.org>
2014-10-01 10:07:26 -0700

    getting floating to work

--- src/main/webapp/WEB-INF/themes/desktop/resource/js/pages/article_body.js
+++ src/main/webapp/WEB-INF/themes/desktop/resource/js/pages/article_body.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
 
   /// write article nav
-  $article = $('#artText');
+ var $article = $('#artText');
 
   $('#nav-article').buildNav({
     content: $article
   });
 
   $('#nav-article').floatingNav({
-    sections: $('#toc-block').find('div.section')
+    sections:  $article.find('div.section')
   });
 
 });

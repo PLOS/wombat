5cd59360be6b51d2944d082013ed27b1180f2d2d
Ryan Skonnord <rskonnord@plos.org>
2014-09-30 17:17:01 -0700

    Fix bug caused by merge from master

--- src/main/webapp/WEB-INF/themes/desktop/resource/js/pages/article_body.js
+++ src/main/webapp/WEB-INF/themes/desktop/resource/js/pages/article_body.js
@@ -1,14 +1,16 @@
-$(document).ready(function () {
+(function ($) {
+  $(document).ready(function () {
 
-  /// write article nav
-  $article = $('#artText');
+    /// write article nav
+    $article = $('#artText');
 
-  $('#nav-article').buildNav({
-    content: $article
+    $('#nav-article').buildNav({
+      content: $article
+    });
+
+    $('#nav-article').floatingNav({
+      sections: $('#toc-block').find('div.section')
+    });
+
   });
-
-  $('#nav-article').floatingNav({
-    sections: $('#toc-block').find('div.section')
-  });
-
-});
+})(jQuery);

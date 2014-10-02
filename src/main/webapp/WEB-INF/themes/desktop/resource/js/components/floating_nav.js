5d5328a99d555ec092d2b5720bdc0be838ec99ee
Polina Grinbaum <pgrinbaum@plos.org>
2014-10-02 16:11:53 -0700

    cleaned up variables, cleaned up css

--- src/main/webapp/WEB-INF/themes/desktop/resource/js/components/floating_nav.js
+++ src/main/webapp/WEB-INF/themes/desktop/resource/js/components/floating_nav.js
@@ -1,9 +1,10 @@
-;(function ($) {
+;
+(function ($) {
   var $win = $(window);
   $.fn.floatingNav = function (options) {
     defaults = {
-      margin:90,
-      sections:''
+      margin:   90,
+      sections: ''
     };
     var options = $.extend(defaults, options);
     return this.each(function () {
@@ -31,31 +32,29 @@
         });
       }
 
-
       var positionEl = function () {
         win_top = $win.scrollTop();
         ftr_top = $('#pageftr').offset().top;
-        if (
-            (win_top > (el_top - options.margin)) //the top of the element is out of the viewport
-                && ((el_h + options.margin + bnr_h) < $win.height()) //the viewport is tall enough-
-                && (win_top < (ftr_top - (el_h + options.margin))) //the element is not overlapping the footer
-                && ($win.width() >= 960) //the viewport is wide enough
-            ) {
-          $this.css({ 'position':'fixed', 'top':options.margin + 'px' });
+
+        var el_view_out = (win_top > (el_top - options.margin)),  //the top of the element is out of the viewport
+            view_height = ((el_h + options.margin + bnr_h) < $win.height()), //the viewport is tall enough-
+            el_overlap = (win_top < (ftr_top - (el_h + options.margin))), //the element is not overlapping the footer
+            view_width = ($win.width() >= 960); //the viewport is wide enough
+
+        if (el_view_out && view_height && el_overlap && view_width) {
+
+          $this.css({ 'position': 'fixed', 'top': options.margin + 'px' });
           hilite();
-        }
-        else {
-          if (win_top > (ftr_top - (el_h + options.margin))) {
-            //Adjust the position here a bit to stop the footer from being overlapped
-            var tt = ftr_top - win_top - el_h - options.margin + 35;
-            $this.css({ 'position':'fixed', 'top':tt + 'px' });
-          } else {
-            //We're above the article
-            $this.css({ 'position':'static'});
-          }
+        } else if (win_top > (ftr_top - (el_h + options.margin))) {
+          //Adjust the position here a bit to stop the footer from being overlapped
+          var tt = ftr_top - win_top - el_h - options.margin + 35;
+          hilite();
+          $this.css({ 'position': 'fixed', 'top': tt + 'px' });
+        } else {
+          //We're above the article
+          $this.css({ 'position': 'static'});
         }
       }
-
 
       var marginFix = function () {
         var lastSection = $('div.article div').last();
@@ -65,7 +64,7 @@
           var z = (docHeight - offset) + options.margin;
           if (z < $win.height()) {
             var margin = Math.ceil(($win.height() - z) + options.margin);
-            lastSection.css({ 'margin-bottom':margin + 'px'});
+            lastSection.css({ 'margin-bottom': margin + 'px'});
           }
         }
       }

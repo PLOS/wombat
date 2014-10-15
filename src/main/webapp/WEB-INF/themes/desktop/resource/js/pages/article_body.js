fcb2b643ad0af77a2287a7ff1eb4cee3a30148d1
Polina Grinbaum <pgrinbaum@plos.org>
2014-10-15 16:49:32 -0700

    wip for target

--- src/main/webapp/WEB-INF/themes/desktop/resource/js/pages/article_body.js
+++ src/main/webapp/WEB-INF/themes/desktop/resource/js/pages/article_body.js
@@ -20,26 +20,36 @@
           });
     }
   }
-//  var hash = window.location.hash.substring(1);
-//  console.log("hash : " + hash);
+  var hash = window.location.hash.substring(1);
+  console.log("hash : " + hash);
 
-    $('.article-body').click(function(event) {
+    $('.article-body a[href^="#"]').click(function(event) {
+
       var $target = $(event.target),
           target_hash = $(event.target.hash),
+          target_hash2 = $target.attr('href').substring(1),
           title_height = $('.topVisible').innerHeight();
-      console.log("target_hash : " + target_hash);
-      if (target_hash) {
+
+      console.log("$target : " + $target);
+      console.log("target_hash2 : " + target_hash2);
+
+
         var target_hash_data = $target.attr('href'),
             target_link = target_hash_data.replace('#', ''),
-            a_tag = $("a[name='"+ target_link +"']");
+            a_tag = $("a[name='"+ target_link +"']"),
+            a_tag_position = a_tag.offset().top;
+      console.log("a_tag_position : " + a_tag_position);
+        console.log("height : " + title_height);
 
-      var targetOffset = a_tag.offset().top - (title_height + 20 );
+      var targetOffset = (a_tag_position + title_height);
+      console.log("targetOffset : " + targetOffset);
         event.preventDefault();
       $('body').animate({scrollTop: targetOffset}, 400, function() {
         location.hash = target_hash_data;
+        alert ($(document).scrollTop().valueOf());
 
       });
-      };
+
     });
 
 

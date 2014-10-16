677f622112cfa88f6291e13b1ef7c4853ad958d9
danadowell <ddowell@plos.org>
2014-10-16 15:18:51 -0700

    dpro-407: twitter module wip

--- src/main/webapp/WEB-INF/themes/desktop/resource/js/components/twitter_module.js
+++ src/main/webapp/WEB-INF/themes/desktop/resource/js/components/twitter_module.js
@@ -12,33 +12,6 @@
       return doi.replace(new RegExp('/', 'g'), '%2F').replace(new RegExp(':', 'g'), '%3A');
     }
 
-
-   /* function getCitesTwitterOnly(doi) {
-      doi = validateDOI(doi);
-
-      var request = doi + '&source=twitter&info=event';
-
-      var config = ALM_CONFIG;
-
-      var requestUrl = 'http://alm.plos.org/api/v3/articles?api_key=' + config.apiKey + '&ids=' + request;
-
-      $.ajax({
-          url: requestUrl,
-          json: 'callback',
-          dataType: 'json',
-          success: function(responseText) {
-            console.log(responseText.data[0].sources[0]);
-            whata = responseText.data[0].sources[0]
-            return whata; //responseText.data[0].sources[0];
-          },
-        failure: function () {
-          console.log('ALM request failed: ' + errorText+ " "+ url);
-          $('#twitter-alm-timeline').append(errorText);
-        }
-      });
-    }*/
-
-
     this.displayTweetsArticleSidebar = function (doi) {
       doi = validateDOI(doi);
 
@@ -61,35 +34,133 @@
         },
         failure: function () {
           console.log('ALM request failed: ' + errorText+ " "+ url);
-          $('#twitter-alm-timeline').append(errorText);
+          $('#twitter-aside').append(errorText);
         }
       });
 
-    }
+    };
+
+    // linkify is from Ambra hallelujah
+    var linkify = function (tweetText) {
+      //Add an extra space so we capture urls/tags/usernames that end on the final character
+      tweetText = tweetText + " ";
+
+      //Replace URLs with a real link
+      var urlRegex = /((ht|f)tp(s?):\/\/[a-zA-Z0-9\-\.\/]+?)([\s])/g;
+      var newValue = tweetText.replace(urlRegex, function (match, url, eol, offset, original) {
+        return "<a href=\"" + url + "\">" + url + "</a> ";
+      });
+
+      //Replace tags with a link to a search for the tag
+      var tagRegex = /#([A-Za-z0-9/-]+?)([:\s])/g;
+      newValue = newValue.replace(tagRegex, function (match, tag, spacer, offset, original) {
+        return "<a href=\"https://twitter.com/#!/search/%23" + tag + "\">#" + tag + "</a>" + spacer;
+      });
+
+      //Replace Retweet username with a link to the user profile
+      var usernameRegex = /@([A-Za-z0-9_]+)([:\s])/g;
+      newValue = newValue.replace(usernameRegex, function (match, username, spacer, offset, original) {
+        return "<a href=\"https://twitter.com/" + username + "\">@" + username + "</a>" + spacer;
+      });
+
+      return newValue;
+    };
 
 
 
 
-    var showTweetsArticleSidebar = function (stuff) {
+    var showTweetsArticleSidebar = function (almTweetData) {
 
 
 
-      var twitterResponse, events, totalEventCount, minDisplayEventCount, maxDisplayEventCount, ol, i,
-        tweet, created_dt, li, tweetText, div, ul, replyLink, reTweetLink, favoriteLink, tweetPostTime,
-        linkToTweet;
+      var events, totalTweets, minDisplayEventCount, maxDisplayEventCount, ol, i,
+        tweet, created_dt, li, tweetText, div, ul, replyLink, reTweetLink, favoriteLink,        linkToTweet,
+      tweetImg,
+        tweetImgSwitch,
+        tweetDate,
+        tweetInfo,
+        tweetActionLink,
+        tweetAvatar,
+        tweetPlaceholder,
+        tweetId,
+        replyLink,
+        retweetLink,
+        favoriteLink,
+        tweetActions,
+        tweetAvatarSrc;
 
       minDisplayEventCount = 2;
       maxDisplayEventCount = 5;
 
-      //assuming here the request was properly formatted to get only twitter in sources[0]
-      twitterResponse = stuff;
-
-      if (twitterResponse && twitterResponse.metrics.total > 0) {
-        $('#twitter-alm-timeline').append(twitterResponse.events[0].event.text);
+      totalTweets = almTweetData.metrics.total;
+      tweetDate = dateParse(almTweetData.events[0].event.created_at, true);
+      tweetAvatar = almTweetData.events[0].event.user_profile_image;
+      tweetPlaceholder = 'resource/img/icon.avatar.placeholder.png';
+      /*var checkAvatar = function (){
+        var image = new Image();
+       // if (image.onerror()===true){console.log('error');}
+        image.onload = function () {
+          tweetAvatarSrc = tweetAvatar;
+          console.log(tweetAvatarSrc);
+         //return tweetAvatarSrc;
+        }
+        image.onerror = function () {
+          console.log(this);
+         tweetAvatarSrc = tweetPlaceholder;
+          console.log(tweetAvatarSrc);
+          return tweetAvatarSrc
+        }
+        image.src = tweetAvatar;
       }
+       checkAvatar();*/
+      /*  $(tweetAvatar).on('load',function(){
+          tweetAvatarSrc = tweetAvatar;
+        }).on('error', function(){
+          tweetAvatarSrc = tweetPlaceholder;
+          console.log(tweetAvatarSrc);
+        })*/
+      // user photo, date of post, user names
+      tweetInfo = '<a href="http://twitter.com/'+almTweetData.events[0].event.user+'"'+'>' +
+      '<img src="'+tweetPlaceholder+'"/>' +
+      '<div class="tweetDate">'+tweetDate+'</div>' +
+      '<div class="tweetUser"><span>'+almTweetData.events[0].event.user_name+'</span> ' +
+        almTweetData.events[0].event.user+'</div></a>';
+
+      // actual tweet content
+      tweetText = linkify(almTweetData.events[0].event.text);
+
+      //twitter reply/retweet/favorite links
+      tweetActionLink = 'https://twitter.com/intent/';
+      tweetId = almTweetData.events[0].event.id;
+      replyLink   = tweetActionLink + 'favorite?in_reply_to&tweet_id=' + tweetId;
+      retweetLink = tweetActionLink + 'retweet?tweet_id='+ tweetId;
+      favoriteLink =  tweetActionLink + 'favorite?tweet_id=' + tweetId;
+
+      tweetActions = '<a class="tweet-reply" href="'+ replyLink + '"><div>&nbsp;</div>Reply</a>' +
+        '<a class="tweet-retweet" href="' + retweetLink + '"><div>&nbsp;</div>Retweet</a>'+
+        '<a class="tweet-favorite" href="' + favoriteLink + '"><div>&nbsp;</div>Favorite</a>';
+
+    // tweetUserLink = '<a href="http://twitter.com/'+almTweetData.events[0].event.user+'"';
+      tweet =
+        '<ul class="tweetList"><li>' +
+        '<div class="tweet-info">'+tweetInfo+'</div>' +
+        '<div class="tweetText">'+tweetText+'</div>' +
+          '<div id="tweetActions">' + tweetActions + '</div>'+
+          '</li></ul>';
+
+      if (almTweetData && almTweetData.metrics.total > 0) {
+        $('#twitter-aside').append(tweet);
+      }
+     /* $('.tweetList #tweetActions').hover(function(){
+         this.addClass('active');
+        this.removeClass('active');
+      });*/
 
       /*<li class="tweet-entry display"><div class="tweet-user"><div class="twitter-posttime"><a href="https://twitter.com/Secular_Respons/statuses/512069241311592448">16 Sep</a></div><a href="https://twitter.com/Secular_Respons"><img src="http://pbs.twimg.com/profile_images/484591619232903170/szpq-wiD_normal.jpeg"><div><span class="twitter-fullname">Secular Response</span><span class="twitter-username">@Secular_Respons</span></div></a></div><div class="tweet-text">Mentalizing Deficits Constrain Belief in a Personal #God. Higher <a href="https://twitter.com/#!/search/%23autism">#autism</a> scores predictive of lower belief.
-       <a href="http://t.co/ihbbVVT7nu">http://t.co/ihbbVVT7nu</a> </div><div><ul class="tweet-actions"><li><a href="https://twitter.com/intent/favorite?in_reply_to=512069241311592448" class="tweet-reply-action" title="Reply"><span></span>Reply</a></li><li><a href="https://twitter.com/intent/retweet?tweet_id=512069241311592448" class="tweet-retweet-action" title="Retweet"><span></span>Retweet</a></li><li><a href="https://twitter.com/intent/favorite?tweet_id=512069241311592448" class="tweet-favorite-action" title="Favorite"><span></span>Favorite</a></li></ul></div></li>*/
+       <a href="http://t.co/ihbbVVT7nu">http://t.co/ihbbVVT7nu</a> </div><div><ul class="tweet-actions">
+       <li><a href="https://twitter.com/intent/favorite?in_reply_to=512069241311592448" class="tweet-reply-action" title="Reply"><span></span>Reply</a></li>
+       <li><a href="https://twitter.com/intent/retweet?tweet_id=512069241311592448" class="tweet-retweet-action" title="Retweet"><span></span>Retweet</a></li>
+       <li><a href="https://twitter.com/intent/favorite?tweet_id=512069241311592448" class="tweet-favorite-action" title="Favorite"><span></span>Favorite</a></li></ul></div></li>*/
       /*  events = twitterResponse.events;
         events = events.sort(jQuery.proxy(this.sort_tweets_by_date, this));
 

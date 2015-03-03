5aa2460438aec7961daa792861b35dc338145d34
Polina Grinbaum <pgrinbaum@plos.org>
2015-03-03 13:10:43 -0800

    DPRO-334 style adjustments

--- src/main/webapp/WEB-INF/themes/desktop/resource/js/components/floating_nav.js
+++ src/main/webapp/WEB-INF/themes/desktop/resource/js/components/floating_nav.js
@@ -1,8 +1,8 @@
-;
+
 (function ($) {
   var $win = $(window);
   $.fn.floatingNav = function (options) {
-    defaults = {
+    var defaults = {
       margin:               90,
       content:              '',
       ulClass:              'nav-page',
@@ -11,15 +11,14 @@
       classActive:          'active',
       footer:               '#pageftr',
       alternateBottomDiv:   '#banner-ftr',
-      linkSelector:         'a.scroll',
-      alternativeBottomDiv: '#banner-ftr'
+      linkSelector:         'a.scroll'
 
     };
-    var options = $.extend(defaults, options);
+    var opts = $.extend(defaults, options);
 
     $('body').on(
-        "click", "nav ul." + options.ulClass + " a", function (event) {
-          var link = $(this);
+        'click', 'ul.' + opts.ulClass + ' a', function (event) {
+
           //window.history.pushState is not on all browsers
           if (window.history.pushState) {
             window.history.pushState({}, document.title, event.target.href);
@@ -28,60 +27,73 @@
           event.preventDefault();
           // suppress distracting expansion/collapse of any nested list items during animation-triggered
           // traversal of the nav
-          $('ul.' + options.ulClass + ' li:not(.' + options.classActive + ') ul').addClass('suppress_expansion');
+          $('ul.' + opts.ulClass + ' li:not(.' + opts.classActive + ') ul').addClass('suppress_expansion');
 
           $('html,body').animate(
-              {scrollTop: $('#' + this.hash.substring(1)).offset().top}, 500, // re-enable expansion/collapse behavior of nested list items (for use during page scrolling)
-              function () {$('ul.' + options.ulClass + ' li ul').removeClass('suppress_expansion')}
+              {scrollTop: $('#' + this.hash.substring(1)).offset().top}, 500, 
+              function () {
+                $('ul.' + opts.ulClass + ' li ul').removeClass('suppress_expansion');
+              }
           );
         }
     );
     return this.each(
         function () {
 
-          var $this = $(this), ftr_top = $(options.footer).offset().top, el_h = $this.innerHeight(), bnr_h = 0, win_top = 0, links = $this.find(options.linkSelector);
+          var $this = $(this);
+          var $ftr_top = $(opts.footer).offset().top;
+          var $el_h = $this.innerHeight();
+          var $links = $this.find(opts.linkSelector);
+          var bnr_h = 0;
+          var win_top = 0;
 
-          if ($(options.alternativeBottomDiv).length) {
-            bnr_h = $(options.alternativeBottomDiv).innerHeight();
+          if ($(opts.alternativeBottomDiv).length) {
+            bnr_h = $(opts.alternativeBottomDiv).innerHeight();
           }
 
           var hilite = function () {
             win_top = $win.scrollTop();
-            options.content.find(options.sectionAnchor).each(
+            opts.content.find(opts.sectionAnchor).each(
                 function () {
-                  this_a = $(this);
-                  if (win_top > (this_a.offset().top - options.margin)) {
-                    var this_a_ref = this_a.attr(options.sectionAnchorAttr);
-                    var $closest_li = $this.find('a[href="#' + this_a_ref + '"]').closest('li');
-                    links.closest('li').removeClass(options.classActive);
-                    $closest_li.addClass(options.classActive);
-                    $closest_li.parents('li').addClass(options.classActive);
+                  var $this_a = $(this);
+                  if (win_top > ($this_a.offset().top - opts.margin)) {
+                    
+                    var $this_a_ref = $this_a.attr(opts.sectionAnchorAttr);
+                    var $closest_li = $this.find('a[href="#' + $this_a_ref + '"]').closest('li');
+                    
+                    $links.closest('li').removeClass(opts.classActive);
+                    $closest_li.addClass(opts.classActive);
+                    $closest_li.parents('li').addClass(opts.classActive);
 
                   } else { }
                 }
             );
 
-          }
+          };
 
           var positionEl = function () {
 
             win_top = $win.scrollTop();
-            ftr_top = $(options.footer).offset().top;
+            $ftr_top = $(opts.footer).offset().top;
+            var article_top = opts.content.offset().top;
+            //the top of the element is out of the viewport
+            var el_view_out = (win_top > (article_top - opts.margin));
+            //the viewport is tall enough-
+            var view_height = (($el_h + opts.margin + bnr_h) < $win.height());
+            //the element is not overlapping the footer
+            var el_overlap = (win_top < ($ftr_top - ($el_h + opts.margin)));
+            //the viewport is wide enough
+            var view_width = ($win.width() >= 960);
 
-            var article_top = options.content.offset().top, el_view_out = (win_top > (article_top - options.margin)),  //the top of the element is out of the viewport
-                view_height = ((el_h + options.margin + bnr_h) < $win.height()), //the viewport is tall enough-
-                el_overlap = (win_top < (ftr_top - (el_h + options.margin))), //the element is not overlapping the footer
-                view_width = ($win.width() >= 960); //the viewport is wide enough
             if (view_height && view_width) {
 
               if (el_view_out && el_overlap) {
-
-                $this.css({'position': 'fixed', 'top': options.margin + 'px'});
+                $this.css({'position': 'fixed', 'top': opts.margin + 'px'});
                 hilite();
+              } else if (win_top > ($ftr_top - ($el_h + opts.margin))) {
 
-              } else if (win_top > (ftr_top - (el_h + options.margin))) {
                 //Adjust the position here a bit to stop the footer from being overlapped
-                var tt = ftr_top - win_top - el_h - options.margin + 35;
+                var tt = $ftr_top - win_top - $el_h - opts.margin + 35;
                 hilite();
                 $this.css({'position': 'fixed', 'top': tt + 'px'});
               } else {
@@ -92,7 +104,7 @@
               $this.css({'position': 'static'});
               hilite();
             }
-          }
+          };
 
           if ($('html.no-touch').length === 1) { // it is not a touch screen
             positionEl();

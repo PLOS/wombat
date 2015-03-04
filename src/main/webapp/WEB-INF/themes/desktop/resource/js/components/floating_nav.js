c0bafaf00174f5a9ef6c55928b18cadceffdd264
Polina Grinbaum <pgrinbaum@plos.org>
2015-03-04 14:12:26 -0800

    WIP

--- src/main/webapp/WEB-INF/themes/desktop/resource/js/components/floating_nav.js
+++ src/main/webapp/WEB-INF/themes/desktop/resource/js/components/floating_nav.js
@@ -3,6 +3,9 @@
   $.fn.floatingNav = function (options) {
     var defaults = {
       margin:               90,
+      reading_space:               35,
+      page_width:           960,
+      scroll_speed: 500,
       content:              '',
       ul_class:             'nav-page',
       section_anchor:       'a[data-toc]',
@@ -10,7 +13,9 @@
       class_active:         'active',
       footer:               '#pageftr',
       alternate_bottom_div: '#banner-ftr',
-      link_selector:        'a.scroll'
+      link_selector:        'a.scroll',
+
+
 
     };
     var opts = $.extend(defaults, options);
@@ -29,8 +34,9 @@
           $('ul.' + opts.ul_class + ' li:not(.' + opts.class_active + ') ul').hide();
 
           $('html,body').animate(
-              {scrollTop: $('#' + this.hash.substring(1)).offset().top}, 500, function () {
-                $('ul.' + opts.ul_class + ' li ul').show();
+              {scrollTop: $('#' + this.hash.substring(1)).offset().top}, opts.scroll_speed,
+              function () {
+                $('ul.' + opts.ul_class + ' li.' + opts.class_active + ' ul').show();
               }
           );
         }
@@ -60,10 +66,9 @@
                     var $closest_li = $this.find('a[href="#' + $this_a_ref + '"]').closest('li');
 
                     $links.closest('li').removeClass(opts.class_active);
-                    $closest_li.addClass(opts.class_active);
-                    $closest_li.parents('li').addClass(opts.class_active);
+                    $closest_li.addClass(opts.class_active).parents('li').addClass(opts.class_active);
 
-                  } else { }
+                  }
                 }
             );
 
@@ -81,7 +86,7 @@
             //the element is not overlapping the footer
             var el_overlap = (win_top < ($ftr_top - ($el_h + opts.margin)));
             //the viewport is wide enough
-            var view_width = ($win.width() >= 960);
+            var view_width = ($win.width() >= opts.page_width);
 
             if (view_height && view_width) {
 
@@ -91,7 +96,7 @@
               } else if (win_top > ($ftr_top - ($el_h + opts.margin))) {
 
                 //Adjust the position here a bit to stop the footer from being overlapped
-                var tt = $ftr_top - win_top - $el_h - opts.margin + 35;
+                var tt = $ftr_top - win_top - $el_h - opts.margin + opts.reading_space;
                 hilite();
                 $this.css({'position': 'fixed', 'top': tt + 'px'});
               } else {

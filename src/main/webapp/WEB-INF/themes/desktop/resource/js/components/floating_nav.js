734489f12890e7428486515b4d7511881e49ff20
Polina Grinbaum <pgrinbaum@plos.org>
2015-03-04 10:50:28 -0800

    Revert 2f5912e..a2eaf99
    This rolls back to commit 2f5912e8471405bddd43a45aeab34af6652447f0.

--- src/main/webapp/WEB-INF/themes/desktop/resource/js/components/floating_nav.js
+++ src/main/webapp/WEB-INF/themes/desktop/resource/js/components/floating_nav.js
@@ -1,120 +1,110 @@
+;
 (function ($) {
   var $win = $(window);
   $.fn.floatingNav = function (options) {
-    var defaults = {
-      margin:               90,
-      content:              '',
-      ul_class:             'nav-page',
-      section_anchor:       'a[data-toc]',
-      section_anchor_attr:  'data-toc',
-      class_active:         'active',
-      footer:               '#pageftr',
-      alternate_bottom_div: '#banner-ftr',
-      link_selector:        'a.scroll'
+    defaults = {
+      margin:             90,
+      content:            '',
+      ulClass:           'nav-page',
+      sectionAnchor:      'a[data-toc]',
+      sectionAnchorAttr:  'data-toc',
+      classActive:        'active',
+      footer:             '#pageftr',
+      alternateBottomDiv: '#banner-ftr',
+      linkSelector:       'a.scroll',
+      alternativeBottomDiv: '#banner-ftr'
 
     };
-    var opts = $.extend(defaults, options);
+    var options = $.extend(defaults, options);
 
-    $('body').on(
-        'click', 'ul.' + opts.ul_class + ' a',
-        function (event) {
+      $('body').on("click", "nav ul." + options.ulClass + " a", function (event) {
+          var link = $(this);
           //window.history.pushState is not on all browsers
           if (window.history.pushState) {
-            window.history.pushState({}, document.title, event.target.href);
-          } else { }
+              window.history.pushState({}, document.title, event.target.href);
+          } else {  }
 
           event.preventDefault();
           // suppress distracting expansion/collapse of any nested list items during animation-triggered
           // traversal of the nav
-          $('ul.' + opts.ul_class + ' li:not(.' + opts.class_active + ') ul').hide();
+          $('ul.' + options.ulClass + ' li:not(.' + options.classActive + ') ul').addClass('suppress_expansion');
 
-          $('html,body').animate(
-              {scrollTop: $('#' + this.hash.substring(1)).offset().top}, 500, function () {
-                $('ul.' + opts.ul_class + ' li ul').show();
-              }
-          );
+          $('html,body').animate({scrollTop: $('#' + this.hash.substring(1) ).offset().top}, 500,
+              // re-enable expansion/collapse behavior of nested list items (for use during page scrolling)
+              function(){$('ul.' + options.ulClass + ' li ul').removeClass('suppress_expansion')});
+      });
+    return this.each(function () {
+
+      var $this = $(this),
+          ftr_top = $(options.footer).offset().top,
+          el_h = $this.innerHeight(),
+          bnr_h = 0,
+          win_top = 0,
+          links = $this.find(options.linkSelector);
+
+      if ($(options.alternativeBottomDiv).length) {
+        bnr_h = $(options.alternativeBottomDiv).innerHeight();
+      }
+
+      var hilite = function () {
+        win_top = $win.scrollTop();
+        options.content.find(options.sectionAnchor).each(function () {
+          this_a = $(this);
+          if (win_top > (this_a.offset().top - options.margin)) {
+            var this_a_ref = this_a.attr(options.sectionAnchorAttr);
+            var $closest_li = $this.find('a[href="#' + this_a_ref + '"]').closest('li');
+            links.closest('li').removeClass(options.classActive);
+            $closest_li.addClass(options.classActive);
+            $closest_li.parents('li').addClass(options.classActive);
+
+          } else { }
+        });
+
+      }
+
+      var positionEl = function () {
+
+        win_top = $win.scrollTop();
+        ftr_top = $(options.footer).offset().top;
+
+        var article_top = options.content.offset().top,
+            el_view_out = (win_top > (article_top - options.margin)),  //the top of the element is out of the viewport
+            view_height = ((el_h + options.margin + bnr_h) < $win.height()), //the viewport is tall enough-
+            el_overlap = (win_top < (ftr_top - (el_h + options.margin))), //the element is not overlapping the footer
+            view_width = ($win.width() >= 960); //the viewport is wide enough
+        if (view_height && view_width ) {
+
+        if (el_view_out && el_overlap) {
+
+          $this.css({ 'position': 'fixed', 'top': options.margin + 'px' });
+          hilite();
+
+        } else if (win_top > (ftr_top - (el_h + options.margin))) {
+          //Adjust the position here a bit to stop the footer from being overlapped
+          var tt = ftr_top - win_top - el_h - options.margin + 35;
+          hilite();
+          $this.css({ 'position': 'fixed', 'top': tt + 'px' });
+        } else {
+          //We're above the article
+          $this.css({ 'position': 'static'});
         }
-    );
-    return this.each(
-        function () {
-
-          var $this = $(this);
-          var $ftr_top = $(opts.footer).offset().top;
-          var $el_h = $this.innerHeight();
-          var $links = $this.find(opts.link_selector);
-          var bnr_h = 0;
-          var win_top = 0;
-
-          if ($(opts.alternativeBottomDiv).length) {
-            bnr_h = $(opts.alternativeBottomDiv).innerHeight();
-          }
-
-          var hilite = function () {
-            win_top = $win.scrollTop();
-            opts.content.find(opts.section_anchor).each(
-                function () {
-                  var $this_a = $(this);
-                  if (win_top > ($this_a.offset().top - opts.margin)) {
-
-                    var $this_a_ref = $this_a.attr(opts.section_anchor_attr);
-                    var $closest_li = $this.find('a[href="#' + $this_a_ref + '"]').closest('li');
-
-                    $links.closest('li').removeClass(opts.class_active);
-                    $closest_li.addClass(opts.class_active);
-                    $closest_li.parents('li').addClass(opts.class_active);
-
-                  } else { }
-                }
-            );
-
-          };
-
-          var positionEl = function () {
-
-            win_top = $win.scrollTop();
-            $ftr_top = $(opts.footer).offset().top;
-            var article_top = opts.content.offset().top;
-            //the top of the element is out of the viewport
-            var el_view_out = (win_top > (article_top - opts.margin));
-            //the viewport is tall enough-
-            var view_height = (($el_h + opts.margin + bnr_h) < $win.height());
-            //the element is not overlapping the footer
-            var el_overlap = (win_top < ($ftr_top - ($el_h + opts.margin)));
-            //the viewport is wide enough
-            var view_width = ($win.width() >= 960);
-
-            if (view_height && view_width) {
-
-              if (el_view_out && el_overlap) {
-                $this.css({'position': 'fixed', 'top': opts.margin + 'px'});
-                hilite();
-              } else if (win_top > ($ftr_top - ($el_h + opts.margin))) {
-
-                //Adjust the position here a bit to stop the footer from being overlapped
-                var tt = $ftr_top - win_top - $el_h - opts.margin + 35;
-                hilite();
-                $this.css({'position': 'fixed', 'top': tt + 'px'});
-              } else {
-                //We're above the article
-                $this.css({'position': 'static'});
-              }
-            } else {
-              $this.css({'position': 'static'});
-              hilite();
-            }
-          };
-
-          if ($('html.no-touch').length === 1) { // it is not a touch screen
-            positionEl();
-            $win.scroll(positionEl);
-            $win.resize(positionEl);
-          } else {
-            // it is a touch screen only use hilite
-            hilite();
-            $win.scroll(hilite);
-          }
-
+        } else {
+          $this.css({ 'position': 'static'});
+          hilite();
         }
-    );
+      }
+
+      if ($('html.no-touch').length === 1){ // it is not a touch screen
+        positionEl();
+        $win.scroll(positionEl);
+        $win.resize(positionEl);
+      } else {
+      // it is a touch screen only use hilite
+        hilite();
+        $win.scroll(hilite);
+      }
+
+
+    });
   };
 })(jQuery);
--- src/main/webapp/WEB-INF/themes/desktop/resource/js/pages/site_content.js
+++ src/main/webapp/WEB-INF/themes/desktop/resource/js/pages/site_content.js
@@ -4,9 +4,9 @@
 
     $('nav.site-content-nav').floatingNav({
         content: $('.body-content'),
-        link_selector: 'a',
-        section_anchor: 'h2, h3',
-        section_anchor_attr: 'id'
+        linkSelector: 'a',
+        sectionAnchor: 'h2, h3',
+        sectionAnchorAttr: 'id'
     });
 
 

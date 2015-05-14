2eb9669395500728dcf925f06c91b7aadd7fcf20
Polina Grinbaum <anilop>
2015-05-13 18:58:07 -0700

    DPRO-98- simplifying
    1. removed unnecessary fast click references, this doesn’t seem to be
    an issue anymore
    2. removed the unnecessary second invoking of the hide menu
    3. moved the document touch start binding we need to close the menus to
    only bind once within the function

--- src/main/webapp/WEB-INF/themes/desktop/resource/js/components/hover_delay.js
+++ src/main/webapp/WEB-INF/themes/desktop/resource/js/components/hover_delay.js
@@ -11,36 +11,32 @@
     init: function () {
       // kick things off
       var $menu_drop_selector = $('li.has-dropdown');
+      var selected_state = '.hover';
       // if mobile, use modernizer to check for touch events. If so then:
-//      1. add needsclick class so fastclick won't do it's magic
-//      2. use hover instead of hoverIntent - hoverIntent adds timing to the hover we don't want for touch devices. '
-      // 3. we need to invoke the hide action AFTER the click because safari on mobile will persist hover state when you go to another page.
+ //Don't use hoverIntent - hoverIntent adds timing to the hover we don't want for touch devices.
 
       if ($('html.touch').length) {
-        $menu_drop_selector.addClass('needsclick').on('touchstart',
-          function () {
-            $(this).menu_drop('show').siblings().menu_drop('hide');
-          }
-        ).bind('touchstart', function(event){
-              event.stopPropagation(); //Make all touch events stop at the .filters ul.children container element
+        $menu_drop_selector.on(
+            'touchstart', function (event) {
+              $(this).menu_drop('show').
+                  siblings(selected_state).
+                  menu_drop('hide');
+             //  Make all touch events stop at the menu so we don't hide the menu when we want click.
+              event.stopPropagation();
+              //make sure we close the menus when you click anywhere else, but only do ti once so that the touchstart event is not bound for all eternity;
+              $(document).one(
+                  'touchstart', function () {
+                    $menu_drop_selector.menu_drop('hide');
+                  });
             });
 
-        $(document).bind('touchstart', function(e) {
-          $menu_drop_selector.menu_drop('hide'); //Close filters drop-downs if user taps ANYWHERE in the page
-        });
-
-        // invoke the hide menu
-        $('.dropdown a').on({ 'click': function () {
-          $.Deferred().done($menu_drop_selector.menu_drop('hide'));
-        }
-        });
       } else {
         //HoverIntent.js is used for the main navigation delay on hover
         $menu_drop_selector.hoverIntent(
-          function () {$(this).menu_drop('show');},
-          function () {$(this).menu_drop('hide');}
+            function () {$(this).menu_drop('show');},
+            function () {$(this).menu_drop('hide');}
         );
       }
     }
-  }
+  };
 })(jQuery);

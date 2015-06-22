74d07610110ad6b40f1384a2eb15fdf718d4624a
stower <stower@plos.org>
2015-06-22 15:36:43 -0700

    DPRO-396: replace spaces with '+' in mobile taxonomy links

--- src/main/webapp/WEB-INF/themes/mobile/resource/js/taxonomy.js
+++ src/main/webapp/WEB-INF/themes/mobile/resource/js/taxonomy.js
@@ -55,7 +55,7 @@ var TaxonomyBrowser = function () {
 
       var termHtml = $('#subject-term-template').html();
       termHtml = termHtml.replace('__TAXONOMY_TERM_ESCAPED__', leaf);
-      termHtml = termHtml.replace('__TAXONOMY_TERM_LEAF__', leaf);
+      termHtml = termHtml.replace('__TAXONOMY_TERM_LEAF__', leaf.replace(/ /g, '+'));
       termHtml = termHtml.replace('__TAXONOMY_TERM_FULL_PATH__', fullPath);
       var childLinkStyle = 'browse-further browse-right';
       if (terms[i].childCount === 0) {

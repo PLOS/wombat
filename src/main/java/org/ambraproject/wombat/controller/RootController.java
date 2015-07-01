985bf96f9f72398cdc5dd2c37ae0bbe336eea3e3
John Fesenko <jfesenko@plos.org>
2015-07-01 15:29:59 -0700

    DPRO-1208: created temporary collections homepage controller

--- src/main/java/org/ambraproject/wombat/controller/RootController.java
+++ src/main/java/org/ambraproject/wombat/controller/RootController.java
@@ -1,5 +1,12 @@
 package org.ambraproject.wombat.controller;
 
+import com.google.common.base.Optional;
+import org.ambraproject.wombat.config.site.Site;
+import org.ambraproject.wombat.config.site.SiteMapping;
+import org.ambraproject.wombat.config.site.SiteParam;
+import org.ambraproject.wombat.service.EntityNotFoundException;
+import org.ambraproject.wombat.service.remote.EditorialContentService;
+import org.ambraproject.wombat.util.CacheParams;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +28,9 @@ public class RootController extends WombatController {
   @Autowired
   private ServletContext servletContext;
 
+  @Autowired
+  private EditorialContentService editorialContentService;
+
   private String getResourceAsBase64(String path) throws IOException {
     byte[] bytes;
     try (InputStream stream = servletContext.getResourceAsStream(path)) {
@@ -36,6 +46,7 @@ public class RootController extends WombatController {
    * page is more useful than an error message. But all end-user-facing pages should belong to one of the sites in
    * {@code siteSet}.
    */
+  @SiteMapping(excluded={"DesktopPlosCollections","MobilePlosCollections"}) // temporary (see tempCollectionsHome below)
   @RequestMapping(value = "/", method = RequestMethod.GET)
   public String home(Model model) throws Exception {
     model.addAttribute("siteKeys", siteSet.getSiteKeys());
@@ -47,6 +58,28 @@ public class RootController extends WombatController {
     model.addAttribute("imageCode", getResourceAsBase64("/WEB-INF/themes/root/app/wombat.jpg"));
 
     return "//approot";
+  }
+
+  /**
+   * Home controller for PLOS Collections (which uses site content with a reserved slug called "homepage")
+   * NOTE: This is a temporary solution until DPRO-1238 is completed this sprint
+   * TODO: specify SiteContentController and "/" namespace for DesktopPlosCollections in wombat.yaml config
+   */
+  @SiteMapping(value={"DesktopPlosCollections","MobilePlosCollections"})
+  @RequestMapping(value = "/", method = RequestMethod.GET)
+  public String tempCollectionsHome(Model model, @SiteParam Site site) throws Exception {
+    String repoKey = "desktop.collections.s.homepage";
+
+    String cacheKey = "siteContent_meta:" + repoKey;
+
+    try {
+      // Check for validity of the content repo key prior to rendering page. Return a 404 if no object found.
+      editorialContentService.requestMetadata(CacheParams.create(cacheKey), repoKey, Optional.<Integer>absent());
+    } catch (EntityNotFoundException e) {
+      throw new NotFoundException(e);
+    }
+    model.addAttribute("siteContentRepoKey", repoKey);
+    return site + "/ftl/siteContent/container";
   }
 
 }

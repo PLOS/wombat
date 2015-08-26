f7e0d3ffaee94e796d059b8554d5d80b16f07843
Ryan Skonnord <rskonnord@plos.org>
2015-08-26 13:54:57 -0700

    Auto-format

--- src/main/java/org/ambraproject/wombat/freemarker/SitePageContext.java
+++ src/main/java/org/ambraproject/wombat/freemarker/SitePageContext.java
@@ -93,7 +93,7 @@ public class SitePageContext {
       return buildLinkToSite(targetSite, handlerName, params);
     } catch (Exception e) {
       log.error("Error building link for journalKey:handlerName={}. Error detail:{}",
-              journalKey.concat(":").concat(handlerName), e.getMessage());
+          journalKey.concat(":").concat(handlerName), e.getMessage());
       return buildLinkToSite(site, handlerName, params);
     }
   }
@@ -110,14 +110,14 @@ public class SitePageContext {
         // Technically there is no reason a controller can't be reached using several namespaces, so it seemed best
         // to make this a warning instead of an error condition, however, it's probably best practice not to do so.
         log.warn("Warning building link for site:handlerName={}. Warning detail: {}",
-                targetSite.toString().concat(":").concat(handlerName), String.valueOf(urlPatterns.size()).
-                        concat(" URL patterns returned from handler mapping: ".concat(urlPatterns.toString())).
-                        concat(" Used pattern: ").concat(urlPattern));
+            targetSite.toString().concat(":").concat(handlerName), String.valueOf(urlPatterns.size()).
+                concat(" URL patterns returned from handler mapping: ".concat(urlPatterns.toString())).
+                concat(" Used pattern: ").concat(urlPattern));
       }
 
       // replace * or ** with the path URI template var to allow expansion when using ANT-style wildcards
       // TODO: support multiple wildcards using {path__0}, {path__1}?
-      urlPattern = urlPattern.replaceFirst("\\*+","{".concat(PATH_TEMPLATE_VAR).concat("}"));
+      urlPattern = urlPattern.replaceFirst("\\*+", "{".concat(PATH_TEMPLATE_VAR).concat("}"));
 
       boolean pathIncludesSiteToken = urlPattern.matches(".*\\{".concat(SITE_TEMPLATE_VAR).concat("\\}.*"));
 
@@ -151,7 +151,7 @@ public class SitePageContext {
 
     } catch (Exception e) {
       log.error("Error building link for site:handlerName={}. Error detail:{}",
-              targetSite.toString().concat(":").concat(handlerName), e.getMessage());
+          targetSite.toString().concat(":").concat(handlerName), e.getMessage());
       return buildLink(DEFAULT_URL_PATH);
     }
   }

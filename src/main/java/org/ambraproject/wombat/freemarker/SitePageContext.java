55e765393d824543030f73ab39fb9bde9568529b
Ryan Skonnord <rskonnord@plos.org>
2015-08-26 14:33:31 -0700

    DPRO-1374: Make error-handling stricter

--- src/main/java/org/ambraproject/wombat/freemarker/SitePageContext.java
+++ src/main/java/org/ambraproject/wombat/freemarker/SitePageContext.java
@@ -17,6 +17,7 @@ import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import java.util.Map;
 import java.util.Set;
 
@@ -32,10 +33,13 @@ public class SitePageContext {
   private final HandlerMappingConfiguration handlerMappingConfig;
   private static final String PATH_TEMPLATE_VAR = "path";
   private static final String SITE_TEMPLATE_VAR = "site";
-  private static final String DEFAULT_URL_PATH = "/";
 
-  private HttpServletRequest extractRequest() throws TemplateModelException {
-    return ((HttpRequestHashModel) environment.getDataModel().get("Request")).getRequest();
+  private HttpServletRequest extractRequest() {
+    try {
+      return ((HttpRequestHashModel) environment.getDataModel().get("Request")).getRequest();
+    } catch (TemplateModelException e) {
+      throw new RuntimeException(e);
+    }
   }
 
   private Site findSite(SiteResolver siteResolver)
@@ -74,13 +78,13 @@ public class SitePageContext {
   }
 
   public String buildLink(SiteSet siteSet, String journalKey, String path) {
+    Site targetSite;
     try {
-      Site targetSite = site.getTheme().resolveForeignJournalKey(siteSet, journalKey);
-      return targetSite.getRequestScheme().buildLink(extractRequest(), path);
-    } catch (Exception e) {
-      log.error("Error building link for path={}. Error detail:{}", path, e.getMessage());
-      return buildLink(path);
+      targetSite = site.getTheme().resolveForeignJournalKey(siteSet, journalKey);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
+    return targetSite.getRequestScheme().buildLink(extractRequest(), path);
   }
 
   public String buildLink(String handlerName, Map<String, Object> params) {
@@ -88,71 +92,65 @@ public class SitePageContext {
   }
 
   public String buildLink(SiteSet siteSet, String journalKey, String handlerName, Map<String, Object> params) {
+    Site targetSite;
     try {
-      Site targetSite = site.getTheme().resolveForeignJournalKey(siteSet, journalKey);
-      return buildLinkToSite(targetSite, handlerName, params);
-    } catch (Exception e) {
-      log.error("Error building link for journalKey:handlerName={}. Error detail:{}",
-          journalKey + ":" + handlerName, e.getMessage());
-      return buildLinkToSite(site, handlerName, params);
+      targetSite = site.getTheme().resolveForeignJournalKey(siteSet, journalKey);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
+    return buildLinkToSite(targetSite, handlerName, params);
   }
 
   private String buildLinkToSite(Site targetSite, String handlerName, final Map<String, Object> params) {
-    try {
-      Set<String> urlPatterns = handlerMappingConfig.getValidPatternsForSite(handlerName, targetSite.getKey());
-      if (urlPatterns.isEmpty()) {
-        log.error("Error building link for site:handlerName={}. Error detail: No patterns returned from handler mapping");
-        return buildLink(DEFAULT_URL_PATH);
-      }
-      String urlPattern = urlPatterns.iterator().next(); // TODO: Sort via AntPathMatcher.AntPatternComparator first?
-      if (urlPatterns.size() > 1) {
-        // Technically there is no reason a controller can't be reached using several namespaces, so it seemed best
-        // to make this a warning instead of an error condition, however, it's probably best practice not to do so.
-        log.warn("Warning building link for site:handlerName={}. Warning detail: {}",
-            targetSite + ":" + handlerName,
-            urlPatterns.size() + " URL patterns returned from handler mapping: " + urlPatterns + " Used pattern: " + urlPattern);
-      }
-
-      // replace * or ** with the path URI template var to allow expansion when using ANT-style wildcards
-      // TODO: support multiple wildcards using {path__0}, {path__1}?
-      urlPattern = urlPattern.replaceFirst("\\*+", "{" + PATH_TEMPLATE_VAR + "}");
-
-      boolean pathIncludesSiteToken = urlPattern.matches(".*\\{" + SITE_TEMPLATE_VAR + "\\}.*");
-
-      UriComponentsBuilder builder = ServletUriComponentsBuilder.fromPath(urlPattern);
-      UriComponents.UriTemplateVariables uriVariables = new UriComponents.UriTemplateVariables() {
-        @Override
-        public Object getValue(String name) {
-          if (!params.containsKey(name)) {
-            if (name.equals(SITE_TEMPLATE_VAR)) {
-              return site.getKey();
-            } else {
-              throw new RuntimeException("Missing required parameter " + name);
-            }
-          }
-          return params.remove(name); // consume params as they are used
-        }
-      };
-
-      String path = builder.build().expand(uriVariables).encode().toString();
-
-      if (!params.isEmpty()) {
-        // any parameters not consumed by the path variable assignment process are assumed to be query params
-        UrlParamBuilder paramBuilder = UrlParamBuilder.params();
-        for (Map.Entry<String, Object> paramEntry : params.entrySet()) {
-          paramBuilder.add(paramEntry.getKey(), paramEntry.getValue().toString());
-        }
-        path = path + "?" + paramBuilder.format();
-      }
-
-      return targetSite.getRequestScheme().buildLink(extractRequest(), path, pathIncludesSiteToken);
-
-    } catch (Exception e) {
-      log.error("Error building link for site:handlerName={}. Error detail:{}",
-          targetSite + ":" + handlerName, e.getMessage());
-      return buildLink(DEFAULT_URL_PATH);
+    Set<String> urlPatterns = handlerMappingConfig.getValidPatternsForSite(handlerName, targetSite.getKey());
+    if (urlPatterns.isEmpty()) {
+      String message = String.format(
+          "Error building link for site:handlerName=%s:%s. Error detail: No patterns returned from handler mapping",
+          targetSite, handlerName);
+      throw new RuntimeException(message);
     }
+    String urlPattern = urlPatterns.iterator().next(); // TODO: Sort via AntPathMatcher.AntPatternComparator first?
+    if (urlPatterns.size() > 1) {
+      // Technically there is no reason a controller can't be reached using several namespaces, so it seemed best
+      // to make this a warning instead of an error condition, however, it's probably best practice not to do so.
+      log.warn("Warning building link for site:handlerName={}. Warning detail: {}",
+          targetSite + ":" + handlerName,
+          urlPatterns.size() + " URL patterns returned from handler mapping: " + urlPatterns + " Used pattern: " + urlPattern);
+    }
+
+    // replace * or ** with the path URI template var to allow expansion when using ANT-style wildcards
+    // TODO: support multiple wildcards using {path__0}, {path__1}?
+    urlPattern = urlPattern.replaceFirst("\\*+", "{" + PATH_TEMPLATE_VAR + "}");
+
+    boolean pathIncludesSiteToken = urlPattern.matches(".*\\{" + SITE_TEMPLATE_VAR + "\\}.*");
+
+    UriComponentsBuilder builder = ServletUriComponentsBuilder.fromPath(urlPattern);
+    UriComponents.UriTemplateVariables uriVariables = new UriComponents.UriTemplateVariables() {
+      @Override
+      public Object getValue(String name) {
+        if (!params.containsKey(name)) {
+          if (name.equals(SITE_TEMPLATE_VAR)) {
+            return site.getKey();
+          } else {
+            throw new RuntimeException("Missing required parameter " + name);
+          }
+        }
+        return params.remove(name); // consume params as they are used
+      }
+    };
+
+    String path = builder.build().expand(uriVariables).encode().toString();
+
+    if (!params.isEmpty()) {
+      // any parameters not consumed by the path variable assignment process are assumed to be query params
+      UrlParamBuilder paramBuilder = UrlParamBuilder.params();
+      for (Map.Entry<String, Object> paramEntry : params.entrySet()) {
+        paramBuilder.add(paramEntry.getKey(), paramEntry.getValue().toString());
+      }
+      path = path + "?" + paramBuilder.format();
+    }
+
+    return targetSite.getRequestScheme().buildLink(extractRequest(), path, pathIncludesSiteToken);
   }
 
 }

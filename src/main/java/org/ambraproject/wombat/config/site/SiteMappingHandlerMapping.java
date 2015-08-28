55bd387c75a797924f345a7d338885b9401a7431
Ryan Skonnord <rskonnord@plos.org>
2015-08-28 11:00:52 -0700

    DPRO-1454: Fix RootController

--- src/main/java/org/ambraproject/wombat/config/site/SiteMappingHandlerMapping.java
+++ src/main/java/org/ambraproject/wombat/config/site/SiteMappingHandlerMapping.java
@@ -1,5 +1,6 @@
 package org.ambraproject.wombat.config.site;
 
+import org.ambraproject.wombat.controller.RootController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,6 +37,9 @@ public class SiteMappingHandlerMapping extends RequestMappingHandlerMapping {
   @Override
   protected RequestCondition<?> getCustomMethodCondition(Method method) {
     RequestMapping methodAnnotation = AnnotationUtils.findAnnotation(method, RequestMapping.class);
+    if (method.getDeclaringClass().equals(RootController.class)) {
+      return handleRootController();
+    }
     if (methodAnnotation == null) {
       return null;
     }
@@ -43,6 +47,32 @@ public class SiteMappingHandlerMapping extends RequestMappingHandlerMapping {
       return null;
     }
     return SiteRequestCondition.create(siteResolver, siteSet, methodAnnotation);
+  }
+
+  /**
+   * Set up a special request condition for {@link RootController}.
+   * <p/>
+   * {@link RootController} is the only pattern-matched page that does not belong to a {@link Site}. ({@link
+   * org.ambraproject.wombat.controller.NotFoundController} also does not belong to a site, but it is not
+   * pattern-matched.) Therefore, it is a special case that we handle uniquely here.
+   * <p/>
+   * In general, we want to serve the root page if the servlet root is not mapped to one or more sites (assuming that
+   * the sites would be served according to special hostnames). Otherwise, we assume that the "/" pattern will be mapped
+   * to {@link org.ambraproject.wombat.controller.HomeController#serveHomepage} by default or something else by
+   * override.
+   *
+   * @return a request condition for the application root pattern if it is available, or {@code null} if that pattern
+   * risks a collision
+   */
+  private RequestCondition<?> handleRootController() {
+    boolean allSitesHavePathToken = true;
+    for (Site site : siteSet.getSites()) {
+      if (!site.getRequestScheme().hasPathToken()) {
+        allSitesHavePathToken = false;
+        break;
+      }
+    }
+    return allSitesHavePathToken ? new PatternsRequestCondition(new String[]{"/"}, null, null, false, false, null) : null;
   }
 
   /**

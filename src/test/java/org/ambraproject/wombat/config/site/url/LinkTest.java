dbef5eefaabf8c8262b03401b367953adddce772
Ryan Skonnord <rskonnord@plos.org>
2015-09-08 14:36:10 -0700

    DPRO-1454: Create LinkTest.java
    Covers Link.toPath with full coverage on Link.Factory cases. Needs more
    coverage on Link.toPattern.
    
    Pulled out StubTheme from an inner class to where it can be reused more.

---
+++ src/test/java/org/ambraproject/wombat/config/site/url/LinkTest.java
@@ -0,0 +1,94 @@
+package org.ambraproject.wombat.config.site.url;
+
+import org.ambraproject.wombat.config.site.Site;
+import org.ambraproject.wombat.config.theme.StubTheme;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
+
+public class LinkTest {
+
+  private static Site dummySite(String siteName, SiteRequestScheme scheme) {
+    return new Site(siteName, new StubTheme(siteName, siteName), scheme);
+  }
+
+  private static final Site TOKEN_1 = dummySite("token1",
+      SiteRequestScheme.builder().setPathToken("site1").build());
+  private static final Site TOKEN_2 = dummySite("token2",
+      SiteRequestScheme.builder().setPathToken("site2").build());
+  private static final Site HOST_AND_TOKEN_1 = dummySite("hostAndToken1",
+      SiteRequestScheme.builder().setPathToken("site3").specifyHost("1.example.com").build());
+  private static final Site HOST_AND_TOKEN_2 = dummySite("hostAndToken2",
+      SiteRequestScheme.builder().setPathToken("site4").specifyHost("2.example.com").build());
+  private static final Site HOST_AND_TOKEN_3 = dummySite("hostAndToken3",
+      SiteRequestScheme.builder().setPathToken("site5").specifyHost("2.example.com").build());
+  private static final Site HOST_ONLY = dummySite("hostOnly",
+      SiteRequestScheme.builder().specifyHost("hostOnly.example.com").build());
+
+  @DataProvider
+  public Object[][] toPathCases() {
+    List<Object[]> cases = new ArrayList<>();
+
+    // Cases where the HttpServletRequest has no interesting properties
+    Object[][] simpleCases = new Object[][]{
+        {Link.toLocalSite(TOKEN_1).toPath(""), "/site1/"},
+        {Link.toLocalSite(TOKEN_1).toPath("path"), "/site1/path"},
+        {Link.toLocalSite(TOKEN_1).toPath("/path"), "/site1/path"},
+        {Link.toLocalSite(TOKEN_1).toPath("path/"), "/site1/path/"},
+        {Link.toLocalSite(TOKEN_1).toPath("/path/"), "/site1/path/"},
+        {Link.toForeignSite(TOKEN_1, TOKEN_2).toPath("path"), "/site2/path"},
+        {Link.toForeignSite(TOKEN_1, TOKEN_1).toPath("path"), "/site1/path"},
+        {Link.toForeignSite(TOKEN_1, HOST_AND_TOKEN_1).toPath("path"), "http://1.example.com/site3/path"},
+        {Link.toForeignSite(HOST_AND_TOKEN_1, HOST_AND_TOKEN_2).toPath("path"), "http://2.example.com/site4/path"},
+        {Link.toForeignSite(HOST_AND_TOKEN_2, HOST_AND_TOKEN_3).toPath("path"), "/site5/path"},
+        {Link.toLocalSite(HOST_ONLY).toPath(""), "/"},
+        {Link.toLocalSite(HOST_ONLY).toPath("path"), "/path"},
+        {Link.toForeignSite(TOKEN_1, HOST_ONLY).toPath("path"), "http://hostOnly.example.com/path"},
+    };
+    MockHttpServletRequest simpleReq = new MockHttpServletRequest();
+    for (Object[] simpleCase : simpleCases) {
+      cases.add(new Object[]{(Link) simpleCase[0], simpleReq, (String) simpleCase[1]});
+    }
+
+    // Cases with special HttpServletRequest objects
+
+    MockHttpServletRequest reqWithContextPath = new MockHttpServletRequest();
+    reqWithContextPath.setContextPath("/context");
+    cases.add(new Object[]{Link.toLocalSite(TOKEN_1).toPath("path"), reqWithContextPath, "/context/site1/path"});
+    cases.add(new Object[]{Link.toForeignSite(TOKEN_1, HOST_AND_TOKEN_1).toPath("path"), reqWithContextPath, "http://1.example.com/context/site3/path"});
+
+    MockHttpServletRequest reqWithPort = new MockHttpServletRequest();
+    reqWithPort.setServerPort(8765);
+    cases.add(new Object[]{Link.toLocalSite(TOKEN_1).toPath("path"), reqWithPort, "/site1/path"});
+    cases.add(new Object[]{Link.toForeignSite(TOKEN_1, HOST_AND_TOKEN_1).toPath("path"), reqWithPort, "http://1.example.com:8765/site3/path"});
+    cases.add(new Object[]{Link.toLocalSite(HOST_ONLY).toPath("path"), reqWithPort, "/path"});
+    cases.add(new Object[]{Link.toForeignSite(TOKEN_1, HOST_ONLY).toPath("path"), reqWithPort, "http://hostOnly.example.com:8765/path"});
+
+    MockHttpServletRequest reqSecure = new MockHttpServletRequest();
+    reqSecure.setSecure(true);
+    reqSecure.setScheme("https");
+    reqSecure.setServerPort(443);
+    cases.add(new Object[]{Link.toLocalSite(TOKEN_1).toPath("path"), reqSecure, "/site1/path"});
+    cases.add(new Object[]{Link.toForeignSite(TOKEN_1, HOST_AND_TOKEN_1).toPath("path"), reqSecure, "https://1.example.com/site3/path"});
+    cases.add(new Object[]{Link.toLocalSite(HOST_ONLY).toPath("path"), reqSecure, "/path"});
+    cases.add(new Object[]{Link.toForeignSite(TOKEN_1, HOST_ONLY).toPath("path"), reqSecure, "https://hostOnly.example.com/path"});
+
+    return cases.toArray(new Object[0][]);
+  }
+
+  @Test(dataProvider = "toPathCases")
+  public void testToPath(Link link, MockHttpServletRequest request, String expectedPath) {
+    assertEquals(link.get(request), expectedPath);
+  }
+
+  @Test(expectedExceptions = RuntimeException.class)
+  public void testInvalidToPathCase() {
+    Link.toForeignSite(HOST_AND_TOKEN_1, TOKEN_1);
+  }
+
+}
--- src/test/java/org/ambraproject/wombat/config/theme/TestTheme.java
+++ src/test/java/org/ambraproject/wombat/config/theme/TestTheme.java
@@ -2,56 +2,17 @@ package org.ambraproject.wombat.config.theme;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import com.google.gson.Gson;
-import freemarker.cache.TemplateLoader;
 import org.ambraproject.wombat.config.site.Site;
 import org.ambraproject.wombat.config.site.SiteSet;
 import org.ambraproject.wombat.config.site.url.SiteRequestScheme;
-import org.apache.commons.io.input.ReaderInputStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.StringReader;
-import java.util.Collection;
 import java.util.Map;
 
 import static org.testng.Assert.assertEquals;
 
 public class TestTheme {
-
-  private static abstract class StubTheme extends Theme {
-    public StubTheme(String key) {
-      super(key, ImmutableList.<Theme>of());
-    }
-
-    @Override
-    public TemplateLoader getTemplateLoader() throws IOException {
-      return null;
-    }
-
-    @Override
-    protected InputStream fetchStaticResource(String path) throws IOException {
-      if (path.equals("config/journal.json")) {
-        Map<String, Object> journalConfigMap = getJournalConfigMap();
-        return new ReaderInputStream(new StringReader(new Gson().toJson(journalConfigMap)));
-      }
-      return null;
-    }
-
-    protected abstract Map<String, Object> getJournalConfigMap();
-
-    @Override
-    protected ResourceAttributes fetchResourceAttributes(String path) throws IOException {
-      return null;
-    }
-
-    @Override
-    protected Collection<String> fetchStaticResourcePaths(String root) throws IOException {
-      return null;
-    }
-  }
 
   @DataProvider
   public Object[][] booleans() {
@@ -62,26 +23,20 @@ public class TestTheme {
   public void testResolveForeignJournalKey(final boolean useJournalKeyMap) throws Exception {
     SiteRequestScheme dummyScheme = SiteRequestScheme.builder().build();
 
-    Theme homeTheme = new StubTheme("homeTheme") {
+    Theme homeTheme = new StubTheme("homeTheme", "homeJournal") {
       @Override
       protected Map<String, Object> getJournalConfigMap() {
-        ImmutableMap.Builder<String, Object> map = ImmutableMap.builder();
-        map.put("journalKey", "homeJournal");
+        Map<String, Object> map = super.getJournalConfigMap();
         if (useJournalKeyMap) {
           ImmutableMap<String, String> otherJournals = ImmutableMap.of("targetJournal", "targetSite");
           map.put("otherJournals", otherJournals);
         }
-        return map.build();
+        return map;
       }
     };
     Site homeSite = new Site("homeSite", homeTheme, dummyScheme);
 
-    Theme targetTheme = new StubTheme("targetTheme") {
-      @Override
-      protected Map<String, Object> getJournalConfigMap() {
-        return ImmutableMap.<String, Object>of("journalKey", "targetJournal");
-      }
-    };
+    Theme targetTheme = new StubTheme("targetTheme", "targetJournal");
     Site targetSite = new Site("targetSite", targetTheme, dummyScheme);
 
     SiteSet siteSet = new SiteSet(ImmutableList.of(homeSite, targetSite));

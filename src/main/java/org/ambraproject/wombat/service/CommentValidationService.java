3a522d518ac9d36fda8368d208e5e875d61e5e99
Ryan Skonnord <rskonnord@plos.org>
2015-12-23 14:08:55 -0800

    DPRO-1761: Add JavaDocs to new services

--- src/main/java/org/ambraproject/wombat/service/CommentValidationService.java
+++ src/main/java/org/ambraproject/wombat/service/CommentValidationService.java
@@ -6,6 +6,16 @@ import java.util.Map;
 
 public interface CommentValidationService {
 
-  Map<String, Object> validate(Site site, String title, String body, boolean hasCompetingInterest, String ciStatement);
+  /**
+   * Validate a comment submission.
+   * <p>
+   * The error keys are mapped to front-end code. The map's values describe the error in a way that varies between keys.
+   * The value is {@code true} if there is nothing to describe.
+   * <p>
+   * This is pretty tightly coupled to the front end. Refactoring welcome.
+   *
+   * @return validation errors, or an empty map if comment passes
+   */
+  public abstract Map<String, Object> validate(Site site, String title, String body, boolean hasCompetingInterest, String ciStatement);
 
 }

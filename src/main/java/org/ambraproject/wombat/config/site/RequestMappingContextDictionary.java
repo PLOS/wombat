15f542f139131476a8c3ed0bce20988ac2557a9a
Ryan Skonnord <rskonnord@plos.org>
2016-01-12 16:32:32 -0800

    Prevent errors is same mapping is registered twice

--- src/main/java/org/ambraproject/wombat/config/site/RequestMappingContextDictionary.java
+++ src/main/java/org/ambraproject/wombat/config/site/RequestMappingContextDictionary.java
@@ -2,12 +2,17 @@ package org.ambraproject.wombat.config.site;
 
 import com.google.common.base.Optional;
 import com.google.common.base.Preconditions;
+import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableTable;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Table;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.function.UnaryOperator;
 
 /**
  * A global bean that looks up patterns that have been mapped to request handlers. Ordinarily those patterns are set up
@@ -23,26 +28,44 @@ import java.util.Map;
  * This class is intended to be thread-safe. Writes are synchronized, and the object is immutable while being read.
  */
 public final class RequestMappingContextDictionary {
+  private static final Logger log = LoggerFactory.getLogger(RequestMappingContextDictionary.class);
 
   // While false, this object is in a state to accept writes. Permanently set to true on first call to getPattern.
   private boolean isFrozen = false;
 
   private final Object writeLock = new Object();
 
-  private final ImmutableTable.Builder<String, Site, RequestMappingContext> siteTableBuilder;
-  private final ImmutableMap.Builder<String, RequestMappingContext> globalTableBuilder;
+  private final Table<String, Site, RequestMappingContext> siteTableBuilder;
+  private final Map<String, RequestMappingContext> globalTableBuilder;
 
   // By convention, assign to each of these fields only once, when isFrozen is set to true.
   private ImmutableTable<String, Site, RequestMappingContext> siteTable = null;
   private ImmutableMap<String, RequestMappingContext> globalTable = null;
 
   public RequestMappingContextDictionary() {
-    siteTableBuilder = ImmutableTable.builder();
-    globalTableBuilder = ImmutableMap.builder();
+    siteTableBuilder = HashBasedTable.create();
+    globalTableBuilder = new HashMap<>();
   }
 
   private static String getHandlerName(RequestMappingContext mapping) {
     return Preconditions.checkNotNull(mapping.getAnnotation().name());
+  }
+
+  private void insertMapping(RequestMappingContext mapping,
+                             UnaryOperator<RequestMappingContext> insertionAction) {
+    synchronized (writeLock) {
+      if (isFrozen) {
+        throw new IllegalStateException("Cannot register more methods after directory has been read");
+      }
+      RequestMappingContext previous = insertionAction.apply(mapping);
+      if (previous != null) {
+        if (previous.equals(mapping)) {
+          log.debug("Registered redundant mapping: {}", mapping.getAnnotation().name());
+        } else {
+          throw new RuntimeException("Key collision for mappings: " + mapping.getAnnotation().name());
+        }
+      }
+    }
   }
 
   /**
@@ -59,12 +82,7 @@ public final class RequestMappingContextDictionary {
     Preconditions.checkArgument(!mapping.isSiteless());
     String handlerName = getHandlerName(mapping);
 
-    synchronized (writeLock) {
-      if (isFrozen) {
-        throw new IllegalStateException("Cannot register more methods after directory has been read");
-      }
-      siteTableBuilder.put(handlerName, site, mapping);
-    }
+    insertMapping(mapping, m -> siteTableBuilder.put(handlerName, site, m));
   }
 
   /**
@@ -79,12 +97,7 @@ public final class RequestMappingContextDictionary {
     Preconditions.checkArgument(mapping.isSiteless());
     String handlerName = getHandlerName(mapping);
 
-    synchronized (writeLock) {
-      if (isFrozen) {
-        throw new IllegalStateException("Cannot register more methods after directory has been read");
-      }
-      globalTableBuilder.put(handlerName, mapping);
-    }
+    insertMapping(mapping, m -> globalTableBuilder.put(handlerName, m));
   }
 
   private void buildAndFreeze() {
@@ -95,8 +108,8 @@ public final class RequestMappingContextDictionary {
     if (!isFrozen) {
       synchronized (writeLock) {
         isFrozen = true;
-        siteTable = siteTableBuilder.build();
-        globalTable = globalTableBuilder.build();
+        siteTable = ImmutableTable.copyOf(siteTableBuilder);
+        globalTable = ImmutableMap.copyOf(globalTableBuilder);
       }
     }
   }
@@ -122,7 +135,7 @@ public final class RequestMappingContextDictionary {
       }
     }
     return globalTable.get(handlerName);
-}
+  }
 
 
   public static interface MappingEntry {

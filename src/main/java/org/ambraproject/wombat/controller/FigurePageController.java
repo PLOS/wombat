790accc5156fc948ef7a5d4e930bf402aa49d83d
John Fesenko <jfesenko@plos.org>
2016-01-19 15:45:03 -0800

    DPRO-1795: clean up imports

--- src/main/java/org/ambraproject/wombat/controller/FigurePageController.java
+++ src/main/java/org/ambraproject/wombat/controller/FigurePageController.java
@@ -15,7 +15,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.xml.transform.TransformerException;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;

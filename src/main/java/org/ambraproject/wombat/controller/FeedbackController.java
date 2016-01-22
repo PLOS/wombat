466606520174b64541b6f322e4a1c92515b2a78f
stower <stower@plos.org>
2016-01-21 16:34:21 -0800

    DPRO-2002:test removing redirect

--- src/main/java/org/ambraproject/wombat/controller/FeedbackController.java
+++ src/main/java/org/ambraproject/wombat/controller/FeedbackController.java
@@ -124,9 +124,7 @@ public class FeedbackController extends WombatController {
 
     // TODO: Needs to build URL with Link?
     // TODO: Includes weird extra URL parameters?
-    request.getParameterMap().clear();
-
-    return "redirect:feedback/success";
+    return indicateSuccess(site);
   }
 
   /**

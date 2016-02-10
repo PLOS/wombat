f71020b5b761d04c52c28dd3c6f53bc650179411
Ryan Skonnord <rskonnord@plos.org>
2016-02-09 16:11:12 -0800

    DPRO-1899: Sketch out RSS feed view for articles

--- src/main/java/org/ambraproject/wombat/controller/FeedController.java
+++ src/main/java/org/ambraproject/wombat/controller/FeedController.java
@@ -1,12 +1,20 @@
 package org.ambraproject.wombat.controller;
 
+import com.google.common.collect.ImmutableList;
+import org.ambraproject.wombat.config.site.Site;
+import org.ambraproject.wombat.config.site.SiteParam;
 import org.ambraproject.wombat.rss.WombatFeed;
 import org.ambraproject.wombat.rss.WombatRssViewer;
+import org.ambraproject.wombat.service.remote.ArticleSearchQuery;
+import org.ambraproject.wombat.service.remote.SolrSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+import java.util.Map;
 
 @Controller
 public class FeedController {
@@ -17,10 +25,28 @@ public class FeedController {
   @Autowired
   private WombatRssViewer wombatRssViewer;
 
-  @RequestMapping(name ="feed", value="/feed.*", method = RequestMethod.GET)
-  public ModelAndView getContent() {
+  @Autowired
+  private SolrSearchService solrSearchService;
+
+  @RequestMapping(name = "feed", value = "/feed.*", method = RequestMethod.GET)
+  public ModelAndView getContent(@SiteParam Site site) {
     ModelAndView mav = new ModelAndView();
-    mav.addObject("feeds", myFeed.createFeed());
+
+    // Temporarily hard-coded results. TODO: Wire to actual Solr service.
+    Map<String, ?> dummyResults;
+    try {
+      dummyResults = solrSearchService.search(ArticleSearchQuery.builder()
+          .setQuery("gene")
+          .setJournalKeys(ImmutableList.of("PLoSONE"))
+          .setStart(0).setRows(15)
+          .setSimple(true).build());
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+
+    mav.addObject("site", site);
+    mav.addObject("solrResults", dummyResults.get("docs"));
+
     mav.setView(wombatRssViewer);
     return mav;
   }
--- src/main/java/org/ambraproject/wombat/rss/WombatRssViewer.java
+++ src/main/java/org/ambraproject/wombat/rss/WombatRssViewer.java
@@ -1,46 +1,81 @@
 package org.ambraproject.wombat.rss;
 
+import com.google.common.base.Strings;
 import com.rometools.rome.feed.rss.Channel;
 import com.rometools.rome.feed.rss.Content;
 import com.rometools.rome.feed.rss.Item;
+import org.ambraproject.wombat.config.site.RequestMappingContextDictionary;
+import org.ambraproject.wombat.config.site.Site;
+import org.ambraproject.wombat.config.site.url.Link;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.view.feed.AbstractRssFeedView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class WombatRssViewer extends AbstractRssFeedView {
 
+  @Autowired
+  private RequestMappingContextDictionary requestMappingContextDictionary;
+
   @Override
   protected List<Item> buildFeedItems(Map<String, Object> model,
-      HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+                                      HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+    Site site = (Site) model.get("site");
+    ItemFactory itemFactory = new ItemFactory(httpServletRequest, site);
+    List<Map<String, ?>> solrResults = (List<Map<String, ?>>) model.get("solrResults");
+    return solrResults.stream().map(itemFactory::represent).collect(Collectors.toList());
+  }
 
-    List<Item> items = new ArrayList<>();
-    Object ob = model.get("feeds");
-    if (ob instanceof List){
-      for(int i = 0; i < ((List<?>)ob).size(); i++){
-        Object feedObj = ((List<?>) ob).get(i);
-        WombatFeed myFeed = (WombatFeed)feedObj;
-        Item item = new Item();
-        item.setTitle(myFeed.getTitle());
-        item.setLink(myFeed.getLink());
-        item.setPubDate(myFeed.getPubDate());
-        Content content = new Content();
-        content.setValue(myFeed.getDescription());
-        item.setContent(content);
-        items.add(item);
-      }
+  private class ItemFactory {
+    private final HttpServletRequest request;
+    private final Site site;
+
+    private ItemFactory(HttpServletRequest request, Site site) {
+      this.request = Objects.requireNonNull(request);
+      this.site = Objects.requireNonNull(site);
     }
-    return items;
 
+    public Item represent(Map<String, ?> article) {
+      Item item = new Item();
+      item.setTitle((String) article.get("title"));
+      item.setLink(Link.toAbsoluteAddress(site)
+          .toPattern(requestMappingContextDictionary, "article")
+          .addQueryParameter("id", article.get("id"))
+          .build().get(request));
+      item.setContent(buildContent(article));
+      // TODO: Fill more values
+      return item;
+    }
+
+    private Content buildContent(Map<String, ?> article) {
+      Content content = new Content();
+      // TODO: Fill values
+      return content;
+    }
   }
 
   @Override
   protected void buildFeedMetadata(Map<String, Object> model, Channel feed, HttpServletRequest request) {
-    feed.setTitle("test title");
-    feed.setLink("test link");
-    feed.setDescription("test desc");
+    Site site = (Site) model.get("site");
+    Map<String, Object> feedConfig;
+    try {
+      feedConfig = site.getTheme().getConfigMap("feed");
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+
+    // TODO: Are these always the same, or should they differ from feed to feed?
+    // Maybe a user who is subscribed to several of our feeds would like these fields to help tell the feeds apart.
+    feed.setTitle(site.getJournalName());
+    feed.setDescription(Strings.nullToEmpty((String) feedConfig.get("description")));
+
+    // TODO: Is this supposed to go to root, or to the feed URL?
+    feed.setLink(Link.toAbsoluteAddress(site).toPath("").get(request));
   }
 }

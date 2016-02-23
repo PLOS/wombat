fbec4964fbad57b5dd596bb6aebb615602befab4
Ryan Skonnord <rskonnord@plos.org>
2016-02-23 13:32:04 -0800

    DPRO-2152: Implement comment metadata in RSS

--- src/main/java/org/ambraproject/wombat/feed/CommentFeedView.java
+++ src/main/java/org/ambraproject/wombat/feed/CommentFeedView.java
@@ -4,6 +4,7 @@ import com.google.common.collect.ImmutableList;
 import com.rometools.rome.feed.atom.Category;
 import com.rometools.rome.feed.atom.Entry;
 import com.rometools.rome.feed.atom.Link;
+import com.rometools.rome.feed.rss.Guid;
 import com.rometools.rome.feed.rss.Item;
 import org.ambraproject.wombat.config.site.RequestMappingContextDictionary;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,10 +19,46 @@ public class CommentFeedView extends AbstractFeedView<Map<String, Object>> {
   @Autowired
   private RequestMappingContextDictionary requestMappingContextDictionary;
 
+  private String getCommentUrl(FeedMetadata feedMetadata, String commentId) {
+    return feedMetadata.buildLink(link -> link
+        .toPattern(requestMappingContextDictionary, "articleComments")
+        .addQueryParameter("id", commentId)
+        .build());
+  }
+
+  private String getArticleUrl(FeedMetadata feedMetadata, Map<String, Object> article) {
+    return feedMetadata.buildLink(link -> link
+        .toPattern(requestMappingContextDictionary, "article")
+        .addQueryParameter("id", article.get("doi"))
+        .build());
+  }
+
+  private static String getCreatorDisplayName(Map<String, Object> comment) {
+    Map<String, Object> creator = (Map<String, Object>) comment.get("creator");
+    return (String) creator.get("displayName");
+  }
+
+  private static Date getCommentDate(Map<String, Object> comment) {
+    return Date.from(Instant.parse((String) comment.get("lastModified")));
+  }
+
   @Override
   protected Item createRssItem(FeedMetadata feedMetadata, Map<String, Object> comment) {
+    String commentId = (String) comment.get("annotationUri");
+
     Item item = new Item();
-    // TODO
+    item.setTitle((String) comment.get("title"));
+    item.setLink(getCommentUrl(feedMetadata, commentId));
+    item.setAuthor(getCreatorDisplayName(comment));
+    item.setPubDate(getCommentDate(comment));
+
+    Guid guid = new Guid();
+    guid.setValue(commentId);
+    guid.setPermaLink(false);
+    item.setGuid(guid);
+
+    // TODO: setContent
+
     return item;
   }
 
@@ -34,25 +71,16 @@ public class CommentFeedView extends AbstractFeedView<Map<String, Object>> {
     Entry entry = new Entry();
     entry.setId(commentId);
     entry.setTitle(commentTitle);
+    entry.setAuthors(ImmutableList.of(createAtomPerson(getCreatorDisplayName(comment))));
     createAtomCategory(comment).map(ImmutableList::of).ifPresent(entry::setCategories);
 
-    Link commentLink = createAtomLink(feedMetadata.buildLink(link -> link
-            .toPattern(requestMappingContextDictionary, "articleComments")
-            .addQueryParameter("id", commentId)
-            .build()),
+    Link commentLink = createAtomLink(getCommentUrl(feedMetadata, commentId),
         commentTitle, Optional.empty(), Optional.empty());
-    Link articleLink = createAtomLink(feedMetadata.buildLink(link -> link
-            .toPattern(requestMappingContextDictionary, "article")
-            .addQueryParameter("id", article.get("doi"))
-            .build()),
+    Link articleLink = createAtomLink(getArticleUrl(feedMetadata, article),
         (String) article.get("title"), Optional.of("related"), Optional.empty());
     entry.setAlternateLinks(ImmutableList.of(commentLink, articleLink));
 
-    Map<String, Object> creator = (Map<String, Object>) comment.get("creator");
-    String displayName = (String) creator.get("displayName");
-    entry.setAuthors(ImmutableList.of(createAtomPerson(displayName)));
-
-    Date lastModified = Date.from(Instant.parse((String) comment.get("lastModified")));
+    Date lastModified = getCommentDate(comment);
     entry.setPublished(lastModified);
     entry.setUpdated(lastModified);
 

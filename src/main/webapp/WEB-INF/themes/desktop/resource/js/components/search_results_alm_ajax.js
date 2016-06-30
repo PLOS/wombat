e3dcc1b3b726f831d6c52fee9906df227fa029b6
anilop <pgrinbaum@plos.org>
2016-06-30 13:26:18 -0700

    DPRO-2269 - WIP

--- src/main/webapp/WEB-INF/themes/desktop/resource/js/components/search_results_alm_ajax.js
+++ src/main/webapp/WEB-INF/themes/desktop/resource/js/components/search_results_alm_ajax.js
@@ -1,18 +1,17 @@
 
-
 var SearchResultsALMs;
 
 (function ($) {
+  var $ALMContainer = $('.search-results-alm-container');
 
   SearchResultsALMs = Class.extend({
 
-
-    init: function () {
+  init: function () {
       var that = this;
       var query = new AlmQuery();
       var validator = new AlmQueryValidator({checkSources: false});
       var DOIlist = [];
-      $('.search-results-alm-container').each(function () {
+    $ALMContainer.each(function () {
         var $this = $(this);
         DOIlist.push([$this.data('doi')]);
       });
@@ -34,14 +33,18 @@ var SearchResultsALMs;
               };
 
               var template = _.template($('#searchResultsAlm').html());
-              $(".search-results-alm-container[data-doi='" + doi + "']").html(template(templateData));
+              that.$ALMContainer.attr("data-doi", doi).html(template(templateData));
 
             });
           })
           .fail(function (error) {
-            var template = _.template($('#searchResultsAlmError').html());
-            $('.search-results-alm-container').html(template());
+              that.showErrorMessage();
           });
+
+    },
+    showErrorMessage: function () {
+      var template = _.template($('#searchResultsAlmError').html());
+      $ALMContainer.html(template());
 
     }
   });

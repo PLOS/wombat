5781305d1f9228e2f5ed81a646fb7c9f446bea90
Ian Vieira <ivieira@plos.org>
2016-07-07 20:23:33 -0300

    DPRO-2667 - Scroll to top when open Alert modal

--- src/main/webapp/WEB-INF/themes/desktop/resource/js/pages/search_results_ajax.js
+++ src/main/webapp/WEB-INF/themes/desktop/resource/js/pages/search_results_ajax.js
@@ -225,7 +225,7 @@ var SearchResult;
       this.$searchAlertOpenModalBtnEl.on('click', function (e) {
         e.preventDefault();
         e.stopPropagation();
-
+        window.scrollTo(0, 0);
         that.$searchAlertModalEl.foundation('reveal', 'open');
 
         if(that.$searchAlertModalEl.hasClass('loggedIn')) {

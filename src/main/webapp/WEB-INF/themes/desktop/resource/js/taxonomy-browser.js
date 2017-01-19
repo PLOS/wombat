a6b12d6ecbf53fdffa960ddba2548d014e8d2fb3
Ryan Skonnord <rskonnord@plos.org>
2017-01-19 11:50:13 -0800

    DPRO-3250: Restore license acknowledgement for dependency
    It seems to have been caught in the automatic boilerplate update.

--- src/main/webapp/WEB-INF/themes/desktop/resource/js/taxonomy-browser.js
+++ src/main/webapp/WEB-INF/themes/desktop/resource/js/taxonomy-browser.js
@@ -23,6 +23,18 @@
 // TaxonomyBrowser requires jquery-mousewheel
 // https://github.com/brandonaaron/jquery-mousewheel
 
+/*! Copyright (c) 2013 Brandon Aaron (http://brandonaaron.net)
+ * Licensed under the MIT License (LICENSE.txt).
+ *
+ * Thanks to: http://adomas.org/javascript-mouse-wheel/ for some pointers.
+ * Thanks to: Mathias Bank(http://www.mathias-bank.de) for a scope bug fix.
+ * Thanks to: Seamus Leahy for adding deltaX and deltaY
+ *
+ * Version: 3.1.3
+ *
+ * Requires: 1.2.2+
+ */
+
 (function (e) {
   "function" == typeof define && define.amd ? define(["jquery"], e) : "object" == typeof exports ? module.exports = e : e(jQuery)
 })(function (e) {

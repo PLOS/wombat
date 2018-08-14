712892a4dc6bb71171a3cef0e27149ae26ad2377
anilop <pgrinbaum@plos.org>
2018-08-14 13:26:38 -0700

    Revert "AMBR-574-viewarticle-url"
    This reverts commit 47b23d565d1649a413b3dcbbc88f3181283f7d9b.

--- src/main/webapp/WEB-INF/themes/desktop/resource/js/pages/article_references.js
+++ src/main/webapp/WEB-INF/themes/desktop/resource/js/pages/article_references.js
@@ -46,9 +46,9 @@
 
           var queryStringConcat = (queryStringAuthor ? 'query.author=' + queryStringAuthor + '&' : '')
             + 'query.title=' + queryStringTitle;
-          var crossrefApi = "https://api.crossref.org/works?query=" + queryStringConcat + "&sort=score&rows=1";
+          var crossrefApi = "http://api.crossref.org/works?query=" + queryStringConcat + "&sort=score&rows=1";
           var DOIResolver = 'https://doi.org/';
-          var crossrefSearchString = 'https://search.crossref.org/?q=' + queryStringCit;
+          var crossrefSearchString = 'http://search.crossref.org/?q=' + queryStringCit;
           var articleLink = null;
 
 

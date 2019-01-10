cdddb91ad7b9c13441f2b899caa9e83d1b002701
Erik Hetzner <ehetzner@plos.org>
2019-01-10 10:38:02 -0800

    Remove themes scripts

--- src/themes/config/current-thesaurus.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-echo -n 'plosthes.2016-4'
--- src/themes/content/mvSiteMenu.sh
+++
@@ -1,5 +0,0 @@
-for x in PlosBiology  PlosCollections  PlosCompBiol  PlosGenetics  PlosMedicine  PlosNtds  PlosOne  PlosPathogens
-do
-    mkdir -p $x/ftl/common/siteMenu/
-    mv ../code/mobile/$x/ftl/common/siteMenu/siteMenuFooter.ftl $x/ftl/common/siteMenu/
-done

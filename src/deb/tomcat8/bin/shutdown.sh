08f78296d169b3a3e3c60c4eceaba4769e3429fd
Erik Hetzner <ehetzner@plos.org>
2020-02-28 10:59:27 -0800

    AMBR-1119 Remove debian stuff

--- src/deb/tomcat8/bin/shutdown.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-export CATALINA_BASE="$( dirname "${BASH_SOURCE[0]}" )"/..
-
-/usr/share/tomcat8/bin/shutdown.sh
-echo "Tomcat stopped"
--- src/deb/tomcat8/bin/startup.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-export CATALINA_BASE="$( dirname "${BASH_SOURCE[0]}" )"/..
-/usr/share/tomcat8/bin/startup.sh
-echo "Tomcat started"
